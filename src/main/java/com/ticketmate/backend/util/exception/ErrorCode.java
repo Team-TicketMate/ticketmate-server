ea96fea1c5c4934f572aaa32dd5269c8732f96c0
JiHoon Baek <133375485+Chuseok22@users.noreply.github.com>
2025-01-15 11:34:11 +0900

    [기능추가][로그인/회원가입] 로그인/회원가입 로직 init: feat: 로그인 시 JWT 로그인 필터 작성 #30

--- src/main/java/com/ticketmate/backend/util/exception/ErrorCode.java
+++ src/main/java/com/ticketmate/backend/util/exception/ErrorCode.java
@@ -18,6 +18,16 @@ public enum ErrorCode {
 
   // AUTH
 
+  MISSING_AUTH_TOKEN(HttpStatus.UNAUTHORIZED, "인증 토큰이 필요합니다."),
+
+  INVALID_ACCESS_TOKEN(HttpStatus.UNAUTHORIZED, "유효하지 않은 엑세스 토큰입니다."),
+
+  EXPIRED_ACCESS_TOKEN(HttpStatus.UNAUTHORIZED, "엑세스 토큰이 만료되었습니다."),
+
+  EXPIRED_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED, "리프레시 토큰이 만료되었습니다."),
+
+  // MEMBER
+
   MEMBER_NOT_FOUND(HttpStatus.BAD_REQUEST, "회원을 찾을 수 없습니다.");
 
 
