692497eecff96bd499a9cf2499d09b9bb9ed9493
JiHoon Baek <133375485+Chuseok22@users.noreply.github.com>
2025-01-23 16:20:31 +0900

    [기능추가][로그인/회원가입] 로그인/회원가입 로직 init: docs: controller swagger Tag 추가 #30

--- src/main/java/com/ticketmate/backend/controller/AuthController.java
+++ src/main/java/com/ticketmate/backend/controller/AuthController.java
@@ -3,6 +3,7 @@ package com.ticketmate.backend.controller;
 import com.ticketmate.backend.object.dto.ApiResponse;
 import com.ticketmate.backend.object.dto.SignUpDto;
 import com.ticketmate.backend.service.MemberService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,6 +13,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/auth")
 @RequiredArgsConstructor
+@Tag(
+        name = "인증 관련 API",
+        description = "회원 인증 관련 API 제공"
+)
 public class AuthController implements AuthControllerDocs {
 
     private final MemberService memberService;
