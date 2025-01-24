450fad2512f0e5e66ad3b25903b707e3b2564a6c
JiHoon Baek <133375485+Chuseok22@users.noreply.github.com>
2025-01-24 11:11:48 +0900

    [기능추가][로그인/회원가입] 로그인/회원가입 로직 init: feat: SignUpRequest dto클래스 입력 필드 pattern 적용 #30

--- src/main/java/com/ticketmate/backend/object/dto/SignUpRequest.java
+++ src/main/java/com/ticketmate/backend/object/dto/SignUpRequest.java
@@ -3,6 +3,7 @@ package com.ticketmate.backend.object.dto;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.*;
 
 @ToString
@@ -27,9 +28,11 @@ public class SignUpRequest {
 
     @NotBlank(message = "생년월일을 입력하세요")
     @Schema(defaultValue = "19980114")
+    @Pattern(regexp = "^(19|20)\\d{2}(0[1-9]|1[0-2])(0[1-9]|[12]\\d|3[01])$", message = "생년월일 형식은 YYYYMMDD 입니다")
     private String birth; // 생년월일 (ex.19980114)
 
     @NotBlank(message = "전화번호를 입력하세요")
     @Schema(defaultValue = "01012345678")
+    @Pattern(regexp = "^01[0-9][0-9]{8}$", message = "전화번호 형식은 01012345678 입니다")
     private String phone; // 전화번호
 }
