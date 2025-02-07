a00a4c6b90f437076520c7d9eec505902eae9a4b
Chuseok22 <bjh59629@naver.com>
2025-02-07 11:10:17 +0900

    [기능추가][공연 엔티티] 콘서트 정보 init feat: 공연장 엔티티 dto 추가 #53

---
+++ src/main/java/com/ticketmate/backend/object/postgres/ConcertHall.java
@@ -0,0 +1,37 @@
+package com.ticketmate.backend.object.postgres;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString(callSuper = true)
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+public class ConcertHall extends BasePostgresEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(columnDefinition = "uuid DEFAULT uuid_generate_v4()", updatable = false, nullable = false)
+    private UUID concertHallId;
+
+    // 공연장 이름
+    @Column(nullable = false, unique = true)
+    private String concertHallName;
+
+    // 수용인원
+    private int capacity;
+
+    // 주소
+    private String address;
+
+    // 홈페이지 url
+    private String concertHallUrl;
+}
