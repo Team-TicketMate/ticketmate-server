106d7d15c2b11cb6082aed82625f3c530803682e
정우혁 <alalalalalalall367@gmail.com>
2025-02-19 18:45:22 +0900

    feat(get-portfolio): AdminController 내부 리스트업 API 및 상세조회 API 설계 #89

--- src/main/java/com/ticketmate/backend/controller/admin/AdminController.java
+++ src/main/java/com/ticketmate/backend/controller/admin/AdminController.java
@@ -1,19 +1,26 @@
 package com.ticketmate.backend.controller.admin;
 
 import com.ticketmate.backend.controller.admin.docs.AdminControllerDocs;
-import com.ticketmate.backend.object.dto.concerthall.request.ConcertHallInfoRequest;
-import com.ticketmate.backend.object.dto.concert.request.ConcertInfoRequest;
+import com.ticketmate.backend.object.dto.admin.request.PortfolioSearchRequest;
+import com.ticketmate.backend.object.dto.admin.response.PortfolioForAdminResponse;
+import com.ticketmate.backend.object.dto.admin.response.PortfolioListForAdminResponse;
 import com.ticketmate.backend.object.dto.auth.request.CustomUserDetails;
-import com.ticketmate.backend.service.concerthall.ConcertHallService;
+import com.ticketmate.backend.object.dto.concert.request.ConcertInfoRequest;
+import com.ticketmate.backend.object.dto.concerthall.request.ConcertHallInfoRequest;
+import com.ticketmate.backend.service.admin.AdminService;
 import com.ticketmate.backend.service.concert.ConcertService;
+import com.ticketmate.backend.service.concerthall.ConcertHallService;
 import com.ticketmate.backend.util.log.LogMonitoringInvocation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
@@ -26,6 +33,7 @@ public class AdminController implements AdminControllerDocs {
 
     private final ConcertService concertService;
     private final ConcertHallService concertHallService;
+    private final AdminService adminService;
 
     @Override
     @PostMapping(value = "/concert-hall/save")
@@ -43,9 +51,27 @@ public class AdminController implements AdminControllerDocs {
     @LogMonitoringInvocation
     public ResponseEntity<Void> saveConcertInfo(
             @AuthenticationPrincipal CustomUserDetails customUserDetails,
-            @Valid @ModelAttribute ConcertInfoRequest request) {
+            @ModelAttribute ConcertInfoRequest request) {
         request.setMember(customUserDetails.getMember());
         concertService.saveConcertInfo(request);
         return ResponseEntity.ok().build();
+    }
+
+    @Override
+    @GetMapping(value = "/portfolio/list")
+    @LogMonitoringInvocation
+    public ResponseEntity<Page<PortfolioListForAdminResponse>> getPortfolioList(
+            @AuthenticationPrincipal CustomUserDetails customUserDetails,
+            @ModelAttribute PortfolioSearchRequest request) {
+        return ResponseEntity.ok(adminService.getPortfolioList(request));
+    }
+
+    @Override
+    @GetMapping(value = "/portfolio/list/{portfolio-id}")
+    @LogMonitoringInvocation
+    public ResponseEntity<PortfolioForAdminResponse> getPortfolioInfo(
+            @AuthenticationPrincipal CustomUserDetails customUserDetails,
+            @PathVariable(value = "portfolio-id") UUID portfolioId) {
+        return ResponseEntity.ok(adminService.getPortfolio(portfolioId));
     }
 }
