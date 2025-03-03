86bf7ab731f999092613767da89c9f30b8871794
정우혁 <alalalalalalall367@gmail.com>
2025-03-03 21:54:00 +0900

    feat(fcm-init): FcmTokenSaveRequest 필드값 fmc -> fcm 수정 #116

--- src/main/java/com/ticketmate/backend/object/dto/fcm/request/FcmTokenSaveRequest.java
+++ src/main/java/com/ticketmate/backend/object/dto/fcm/request/FcmTokenSaveRequest.java
@@ -14,7 +14,7 @@ import lombok.*;
 public class FcmTokenSaveRequest {
     @NotBlank(message = "FCM 토큰을 입력해주세요")
     @Schema(defaultValue = "chM0thKFkDxdhMm5WAMMwQ:APA91bGKs8nN4A_bPClciFu88Z2bgN9-gvPKsopGsxPcKS2K86Gu9JcZi0HPHgcwpONymKpTiMPE4ztmIt0qXOl9gKUom13Aze80CkvPE6JwEuAgxJDRGtg")
-    private String fmcToken; // FCM 토큰
+    private String fcmToken; // FCM 토큰
 
     private MemberPlatform memberPlatform;
 }
