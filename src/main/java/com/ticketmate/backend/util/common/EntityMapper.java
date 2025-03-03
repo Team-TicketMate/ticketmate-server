e18ba7c5c081a2c03215a7e39064b01941345966
정우혁 <alalalalalalall367@gmail.com>
2025-03-03 15:53:27 +0900

    feat(fcm-init): FcmTokenSaveResponseDTO 및 EntityMapper 내부 메서드 설계 #116

--- src/main/java/com/ticketmate/backend/util/common/EntityMapper.java
+++ src/main/java/com/ticketmate/backend/util/common/EntityMapper.java
@@ -4,9 +4,11 @@ import com.ticketmate.backend.object.dto.admin.response.PortfolioForAdminRespons
 import com.ticketmate.backend.object.dto.admin.response.PortfolioListForAdminResponse;
 import com.ticketmate.backend.object.dto.concert.response.ConcertFilteredResponse;
 import com.ticketmate.backend.object.dto.concerthall.response.ConcertHallFilteredResponse;
+import com.ticketmate.backend.object.dto.fcm.response.FcmTokenSaveResponse;
 import com.ticketmate.backend.object.postgres.concert.Concert;
 import com.ticketmate.backend.object.postgres.concerthall.ConcertHall;
 import com.ticketmate.backend.object.postgres.portfolio.Portfolio;
+import com.ticketmate.backend.object.redis.FcmToken;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
@@ -31,4 +33,7 @@ public interface EntityMapper {
     @Mapping(source = "member.profileUrl", target = "profileUrl")
     @Mapping(source = "member.memberType", target = "memberType")
     PortfolioForAdminResponse toPortfolioForAdminResponse(Portfolio portfolio);
+
+    // FcmToken -> FcmTokenSaveResponse
+    FcmTokenSaveResponse toFcmTokenSaveResponse(FcmToken fcmToken);
 }
