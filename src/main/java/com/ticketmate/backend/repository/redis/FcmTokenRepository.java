152e658123631bcde76d61353383fbb715b40514
정우혁 <alalalalalalall367@gmail.com>
2025-03-12 17:32:28 +0900

    feat(notification-init): FcmTokenRepository 내부 쿼리메서드 추가 #148

--- src/main/java/com/ticketmate/backend/repository/redis/FcmTokenRepository.java
+++ src/main/java/com/ticketmate/backend/repository/redis/FcmTokenRepository.java
@@ -3,7 +3,9 @@ package com.ticketmate.backend.repository.redis;
 import com.ticketmate.backend.object.redis.FcmToken;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface FcmTokenRepository extends CrudRepository<FcmToken, UUID> {
+    List<FcmToken> findAllByMemberId(UUID memberId);
 }
