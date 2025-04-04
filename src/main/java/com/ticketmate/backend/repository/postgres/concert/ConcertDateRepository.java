28c689c604e68d9ef22a78e44997db1154ad8971
Chuseok22 <bjh59629@naver.com>
2025-04-04 13:36:18 +0900

    [기능요청][신청서] 대리 티켓팅 신청 폼 엔티티 설계 및 로직 작성 refactor: DB조회 시 PK를 사용하여 조회 #75

--- src/main/java/com/ticketmate/backend/repository/postgres/concert/ConcertDateRepository.java
+++ src/main/java/com/ticketmate/backend/repository/postgres/concert/ConcertDateRepository.java
@@ -1,13 +1,16 @@
 package com.ticketmate.backend.repository.postgres.concert;
 
-import com.ticketmate.backend.object.postgres.concert.Concert;
 import com.ticketmate.backend.object.postgres.concert.ConcertDate;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
 public interface ConcertDateRepository extends JpaRepository<ConcertDate, UUID> {
 
-    List<ConcertDate> findAllByConcert(Concert concert);
+    List<ConcertDate> findAllByConcert_ConcertId(UUID concertId);
+
+    // 공연PK + 공연일자로 조회
+    ConcertDate findByConcert_ConcertIdAndPerformanceDate(UUID concertId, LocalDateTime performanceDate);
 }
