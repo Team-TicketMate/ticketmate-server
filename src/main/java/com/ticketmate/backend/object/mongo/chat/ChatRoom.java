d41b0c920638710d2d202220f5dfa2ebcb654c33
정우혁 <alalalalalalall367@gmail.com>
2025-04-21 15:44:48 +0900

    feat(application): ChatRoom 도큐먼트 내부 인덱스 에노테이션 추가 #215

--- src/main/java/com/ticketmate/backend/object/mongo/chat/ChatRoom.java
+++ src/main/java/com/ticketmate/backend/object/mongo/chat/ChatRoom.java
@@ -4,6 +4,7 @@ import com.ticketmate.backend.object.mongo.global.BaseMongoDocument;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.UUID;
@@ -17,8 +18,11 @@ import java.util.UUID;
 public class ChatRoom extends BaseMongoDocument {
     @Id
     private String roomId;  // 채팅방 PK
-    private UUID agentMemberId;  // 의뢰인 PK
-    private UUID clientMemberId;  // 대리인 PK
+    @Indexed
+    private UUID agentMemberId;  // 대리인 PK
+    @Indexed
+    private UUID clientMemberId;  // 의뢰인 PK
+    @Indexed
     private UUID applicationFormId;  // 신청폼 PK
     private String roomName;  // 채팅방 이름
 }
