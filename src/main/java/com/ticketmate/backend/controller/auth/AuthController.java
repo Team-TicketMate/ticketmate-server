f17eea09b0356ade641c8167434a942e599be790
Chuseok22 <bjh59629@naver.com>
2025-04-22 14:28:17 +0900

    로그인 시 리다이렉트 경로 지정 : feat : 리다이렉트 경로 지정 취소 및 배포 redirectUri 고정 https://github.com/Team-TicketMate/ticketmate-server/issues/217

--- src/main/java/com/ticketmate/backend/controller/auth/AuthController.java
+++ src/main/java/com/ticketmate/backend/controller/auth/AuthController.java
@@ -2,13 +2,11 @@ package com.ticketmate.backend.controller.auth;
 
 import com.ticketmate.backend.controller.auth.docs.AuthControllerDocs;
 import com.ticketmate.backend.object.dto.auth.request.CustomOAuth2User;
-import com.ticketmate.backend.service.auth.AuthService;
 import com.ticketmate.backend.service.member.MemberService;
 import com.ticketmate.backend.util.log.LogMonitoringInvocation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -23,7 +21,6 @@ import org.springframework.web.bind.annotation.*;
 public class AuthController implements AuthControllerDocs {
 
     private final MemberService memberService;
-    private final AuthService authService;
 
     @Override
     @PostMapping(value = "/api/auth/reissue")
--- src/main/java/com/ticketmate/backend/util/filter/CustomSuccessHandler.java
+++ src/main/java/com/ticketmate/backend/util/filter/CustomSuccessHandler.java
@@ -1,16 +1,15 @@
 package com.ticketmate.backend.util.filter;
 
 import com.ticketmate.backend.object.dto.auth.request.CustomOAuth2User;
-import com.ticketmate.backend.service.auth.AuthService;
 import com.ticketmate.backend.util.JwtUtil;
 import com.ticketmate.backend.util.common.CookieUtil;
 import com.ticketmate.backend.util.exception.CustomException;
 import com.ticketmate.backend.util.exception.ErrorCode;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
@@ -19,19 +18,22 @@ import org.springframework.stereotype.Component;
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
-import static com.ticketmate.backend.util.common.CommonUtil.nvl;
-
 @Component
 @Slf4j
 @RequiredArgsConstructor
 public class CustomSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
 
-    private final AuthService authService;
     private final JwtUtil jwtUtil;
     private final RedisTemplate<String, Object> redisTemplate;
     private final CookieUtil cookieUtil;
 
     private static final String REFRESH_PREFIX = "RT:";
+
+    @Value("${spring.security.app.redirect-uri.dev}")
+    private String devRedirectUri;
+
+    @Value("${spring.security.app.redirect-uri.prod}")
+    private String prodRedirectUri;
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
@@ -53,26 +55,6 @@ public class CustomSuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
                 TimeUnit.MILLISECONDS
         );
 
-        HttpSession session = request.getSession();
-        String redirectUriKey = (String) session.getAttribute("redirectUriKey");
-        log.debug("세션에서 리다이렉트 KEY를 추출합니다: {}", redirectUriKey);
-
-        if (nvl(redirectUriKey, "").isEmpty()) {
-            log.error("세션에 저장된 리다이렉트 키가 없습니다.");
-            throw new CustomException(ErrorCode.INTERNAL_SERVER_ERROR);
-        }
-
-        // Redis에서 redirectUri 조회
-        String redirectUri = authService.getAndDeleteRedirectUri(redirectUriKey);
-        if (nvl(redirectUri, "").isEmpty()) {
-            log.error("redirectUri가 존재하지 않습니다.");
-            throw new CustomException(ErrorCode.INTERNAL_SERVER_ERROR);
-        }
-        log.debug("요청된 redirectUri: {}", redirectUri);
-
-        // 세션에서 키 제거
-        session.removeAttribute("redirectUriKey");
-
         // 쿠키에 accessToken, refreshToken 추가
         response.addCookie(cookieUtil.createCookie("accessToken", accessToken));
         response.addCookie(cookieUtil.createCookie("refreshToken", refreshToken));
@@ -81,7 +63,7 @@ public class CustomSuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
         try {
             log.debug("로그인 성공, 메인페이지로 리다이렉트 됩니다");
             if (!response.isCommitted()) {
-                response.sendRedirect(redirectUri);
+                response.sendRedirect(prodRedirectUri); // FIXME: 추후 리다이렉트 동적으로 받는 로직 작성
             }
         } catch (IOException e) {
             log.error("로그인 성공 후 리다이렉트 과정에서 문제가 발생했습니다. {}", e.getMessage());
