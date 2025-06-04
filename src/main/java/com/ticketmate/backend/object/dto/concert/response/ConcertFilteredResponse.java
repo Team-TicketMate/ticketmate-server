65a0bcf5e33b2d00c422aa8d304fd9ed4bf9ddce
Chuseok22 <bjh59629@naver.com>
2025-06-04 10:40:59 +0900

    포트폴리오 업로드 및 조회 리팩토링 : feat : 응답 DTO LocalDateTime 형식 지정 https://github.com/Team-TicketMate/ticketmate-server/issues/275

--- src/main/java/com/ticketmate/backend/object/dto/concert/response/ConcertFilteredResponse.java
+++ src/main/java/com/ticketmate/backend/object/dto/concert/response/ConcertFilteredResponse.java
@@ -1,5 +1,6 @@
 package com.ticketmate.backend.object.dto.concert.response;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.ticketmate.backend.object.constants.ConcertType;
 import com.ticketmate.backend.object.constants.TicketReservationSite;
 import lombok.*;
@@ -19,8 +20,10 @@ public class ConcertFilteredResponse {
     private String concertHallName; // 공연장 이름
     private ConcertType concertType; // 공연 카테고리
     private TicketReservationSite ticketReservationSite; // 예매처
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss", timezone = "Asia/Seoul")
     private LocalDateTime ticketPreOpenDate; // 티켓 선예매 오픈일
     private Boolean preOpenBankTransfer; // 선예매 무통장 여부
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss", timezone = "Asia/Seoul")
     private LocalDateTime ticketGeneralOpenDate; // 티켓 일반 예매 오픈일
     private Boolean generalOpenBankTransfer; // 일반 예매 무통장 여부
     private LocalDateTime startDate; // 공연 시작일
