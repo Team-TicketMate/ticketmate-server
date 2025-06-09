cf1355b95544e62dde244fec089e1dbe54a1aa94
정우혁 <93671010+mr6208@users.noreply.github.com>
2025-06-09 19:53:32 +0900

    feat(chat-room): 기존 채팅방 도큐먼트 필드 변경 #194

--- src/main/java/com/ticketmate/backend/object/mongo/chat/ChatRoom.java
+++ src/main/java/com/ticketmate/backend/object/mongo/chat/ChatRoom.java
@@ -1,9 +1,11 @@
 package com.ticketmate.backend.object.mongo.chat;
 
+import com.ticketmate.backend.object.constants.TicketOpenType;
 import com.ticketmate.backend.object.mongo.global.BaseMongoDocument;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.CompoundIndex;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -11,6 +13,15 @@ import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Document(collection = "chat_room")
+// 대리인, 의뢰인, 콘서트, 선예매/일반예매 4가지 필드의 복합 인덱스 (4가지 조건이 갖춰진 데이터는 1개여야함)
+@CompoundIndex(
+        name = "uk_agent_client_concert_preopen",
+        def  = "{'agentMemberId': 1, "
+                + "'clientMemberId': 1, "
+                + "'concertId': 1, "
+                + "'preOpen': 1}",
+        unique = true
+)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 @SuperBuilder
@@ -32,5 +43,22 @@ public class ChatRoom extends BaseMongoDocument {
     @Indexed
     private LocalDateTime lastMessageTime;  // 마지막 메시지가 온 시간
     @Indexed
-    private Boolean preOpen;  // 신청폼의 선예매, 일반예매인지
+    private String lastMessage;  // 마지막 메시지
+    @Indexed
+    private UUID concertId;  // 콘서트
+    @Indexed
+    private String lastMessageId;  // 마지막 메시지의 ID 정보
+    private TicketOpenType preOpen;  // 신청폼의 선예매, 일반예매인지
+
+    public void updateLastMessage(String message) {
+        this.lastMessage = message;
+    }
+
+    public void updateLastMessageTime(LocalDateTime lastMessageTime) {
+        this.lastMessageTime = lastMessageTime;
+    }
+
+    public void updateLastMessageId(String messageId) {
+        this.lastMessageId = messageId;
+    }
 }
