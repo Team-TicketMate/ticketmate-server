b471aef13b00b674c4cb6841914a3a95109fb299
정우혁 <93671010+mr6208@users.noreply.github.com>
2025-06-17 20:37:38 +0900

    feat(fcm_token): FCM 토큰 분기처리를 위한 쿼리메서드 작성

--- src/main/java/com/ticketmate/backend/domain/notification/repository/FcmTokenRepository.java
+++ src/main/java/com/ticketmate/backend/domain/notification/repository/FcmTokenRepository.java
@@ -1,11 +1,12 @@
 package com.ticketmate.backend.domain.notification.repository;
 
 import com.ticketmate.backend.domain.notification.domain.entity.FcmToken;
-import java.util.List;
-import java.util.UUID;
 import org.springframework.data.repository.CrudRepository;
 
-public interface FcmTokenRepository extends CrudRepository<FcmToken, String> {
+import java.util.List;
+import java.util.UUID;
 
+public interface FcmTokenRepository extends CrudRepository<FcmToken, String> {
+  boolean existsByMemberId(UUID memberId);
   List<FcmToken> findAllByMemberId(UUID memberId);
 }
