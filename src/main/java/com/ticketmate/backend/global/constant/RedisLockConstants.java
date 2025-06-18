c745b57c075d2bcab28a5eead668a96b1d6b59fc
Chuseok22 <bjh59629@naver.com>
2025-06-18 17:22:22 +0900

    서비스 내 분산된 상수 관리를 위한 통합 관리 구조 도입 : feat : Redis Lock 관련 상수 분리 https://github.com/Team-TicketMate/ticketmate-server/issues/323

---
+++ src/main/java/com/ticketmate/backend/global/constant/RedisLockConstants.java
@@ -0,0 +1,9 @@
+package com.ticketmate.backend.global.constant;
+
+public class RedisLockConstants {
+
+  // Redis Lock
+  public static final Long WAIT_TIME = 5L; // Lock 획득을 위한 대기 시간
+  public static final Long LEASE_TIME = 2L; // Lock 획득 후 만료 시간
+
+}
