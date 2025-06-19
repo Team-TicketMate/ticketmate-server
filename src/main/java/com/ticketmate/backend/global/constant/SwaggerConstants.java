59c326c281972dff340fc90295693d1e2bd2d828
Chuseok22 <bjh59629@naver.com>
2025-06-19 09:01:03 +0900

    서비스 내 분산된 상수 관리를 위한 통합 관리 구조 도입 : refactor : Swagger 관련 클래스 상수 분리 https://github.com/Team-TicketMate/ticketmate-server/issues/323

---
+++ src/main/java/com/ticketmate/backend/global/constant/SwaggerConstants.java
@@ -0,0 +1,10 @@
+package com.ticketmate.backend.global.constant;
+
+public class SwaggerConstants {
+
+  // Swagger
+  public static final String WEB_SITE_URL = "https://www.ticketmate.site";
+  public static final String MAIN_SERVER_URL = "https://api.ticketmate.site";
+  public static final String TEST_SERVER_URL = "https://test.ticketmate.site";
+  public static final String LOCAL_SERVER_URL = "http://localhost:8080";
+}
