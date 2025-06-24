d03eec4597b9349b5927e2aa5e5e5a560e3114e7
Chuseok22 <bjh59629@naver.com>
2025-06-24 15:46:14 +0900

    재신청 시 중복된 신청서 생성 및 수정 혼동 방지 : refactor : ApplicationFormRequest 내부 필드 spring validate 재적용 https://github.com/Team-TicketMate/ticketmate-server/issues/332

--- src/main/java/com/ticketmate/backend/domain/applicationform/domain/dto/request/ApplicationFormRequest.java
+++ src/main/java/com/ticketmate/backend/domain/applicationform/domain/dto/request/ApplicationFormRequest.java
@@ -20,13 +20,15 @@ import lombok.ToString;
 @Builder
 public class ApplicationFormRequest {
 
+  @NotNull(message = "대리인 PK 값을 입력해주세요")
   private UUID agentId; // 대리인 PK
 
-  private UUID concertId; // 공연 PK
+  @NotNull(message = "콘서트 PK 값을 입력해주세요")
+  private UUID concertId; // 콘서트 PK
 
   @NotEmpty(message = "신청서에는 최소 1개 이상의 신청서 세부사항이 포함되어야 합니다")
   private List<ApplicationFormDetailRequest> applicationFormDetailRequestList; // 신청서 세부사항 List
 
   @NotNull(message = "선예매/일반예매 타입을 입력해주세요")
-  private TicketOpenType ticketOpenType; // 선예매 여부
+  private TicketOpenType ticketOpenType; // 선예매/일반예매 여부
 }
