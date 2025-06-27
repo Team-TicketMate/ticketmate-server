a40db43651ab6891ee7ffdacdd09bafd4565180a
정우혁 <93671010+mr6208@users.noreply.github.com>
2025-06-27 18:07:33 +0900

    feat(채팅메지시_페이지네이션): 페이지네이션을 위한 서비스로직 리팩토링

--- src/main/java/com/ticketmate/backend/domain/chat/repository/ChatMessageRepository.java
+++ src/main/java/com/ticketmate/backend/domain/chat/repository/ChatMessageRepository.java
@@ -1,10 +1,10 @@
 package com.ticketmate.backend.domain.chat.repository;
 
 import com.ticketmate.backend.domain.chat.domain.entity.ChatMessage;
-import java.util.List;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface ChatMessageRepository extends MongoRepository<ChatMessage, String>, ChatMessageRepositoryCustom {
-
-  List<ChatMessage> findByChatRoomIdOrderBySendDateAsc(String chatRoomId);
+  Slice<ChatMessage> findByChatRoomId(String chatRoomId, Pageable pageable);
 }
