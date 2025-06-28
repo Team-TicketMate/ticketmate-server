7fa5804a3df52e190cd90162a140b36831aae1d1
장윤정 <wkddbs3935@naver.com>
2025-06-28 22:02:06 +0900

    feat(agent-concert-onoff): dto 스키마 작성

--- src/main/java/com/ticketmate/backend/domain/concert/domain/dto/request/ConcertAgentAvailabilityRequest.java
+++ src/main/java/com/ticketmate/backend/domain/concert/domain/dto/request/ConcertAgentAvailabilityRequest.java
@@ -1,5 +1,6 @@
 package com.ticketmate.backend.domain.concert.domain.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import java.util.UUID;
 import lombok.AllArgsConstructor;
@@ -15,12 +16,12 @@ import lombok.Setter;
 @NoArgsConstructor
 public class ConcertAgentAvailabilityRequest {
   @NotNull(message = "concertId는 필수입니다.")
+  @Schema(defaultValue = "3fa85f64-5717-4562-b3fc-2c963f66afa6")
   private UUID concertId;
 
-  @NotNull(message = "agentId는 필수입니다.")
-  private UUID agentId;
-
+  @Schema(defaultValue = "true")
   private boolean accepting;
 
+  @Schema(defaultValue = "안녕하세요 저에게 맡겨주세요 !!!")
   private String introduction;
 }
