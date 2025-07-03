6f6af04a93bab7b6e587717bb623a0becb0aa185
정우혁 <93671010+mr6208@users.noreply.github.com>
2025-07-03 12:18:13 +0900

    Test (#385)
    * feat(hotfix): 채팅전용 상수 유틸리티 클래스 설계 및 날짜 포멧팅 통일
    
    * feat(hotfix): 채팅 테스팅 전용 html파일 수정

---
+++ src/main/java/com/ticketmate/backend/global/constant/ChatConstants.java
@@ -0,0 +1,14 @@
+package com.ticketmate.backend.global.constant;
+
+import lombok.experimental.UtilityClass;
+
+import java.time.Duration;
+import java.time.format.DateTimeFormatter;
+
+@UtilityClass
+public class ChatConstants {
+    public static final String LAST_READ_MESSAGE_POINTER_KEY = "userLastRead:%s:%s";
+    public static final String UN_READ_MESSAGE_COUNTER_KEY = "unRead:%s:%s";
+    public static final Duration TTL = Duration.ofDays(30);
+    public static final DateTimeFormatter ISO_SEC = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss");
+}
