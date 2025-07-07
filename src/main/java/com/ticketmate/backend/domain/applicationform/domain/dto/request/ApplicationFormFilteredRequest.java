82ec5333db8fc7613846134ac823c0369295951d
Chuseok22 <bjh59629@naver.com>
2025-07-07 12:37:30 +0900

    filteredRequest DTO sortField enum 관리 및 QueryDSL 마이그레이션 : refactor : 신청서 필터링 조회 QueryDSL 마이그레이션 및 리팩토링 https://github.com/Team-TicketMate/ticketmate-server/issues/393

--- src/main/java/com/ticketmate/backend/domain/applicationform/domain/dto/request/ApplicationFormFilteredRequest.java
+++ src/main/java/com/ticketmate/backend/domain/applicationform/domain/dto/request/ApplicationFormFilteredRequest.java
@@ -1,11 +1,12 @@
 package com.ticketmate.backend.domain.applicationform.domain.dto.request;
 
+import com.ticketmate.backend.domain.applicationform.domain.constant.ApplicationFormSortField;
 import com.ticketmate.backend.domain.applicationform.domain.constant.ApplicationFormStatus;
 import com.ticketmate.backend.global.constant.PageableConstants;
+import com.ticketmate.backend.global.util.database.PageableUtil;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.Pattern;
 import java.util.Set;
 import java.util.UUID;
 import lombok.AllArgsConstructor;
@@ -13,6 +14,9 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
 
 @ToString
 @AllArgsConstructor
@@ -39,19 +43,25 @@ public class ApplicationFormFilteredRequest {
   @Max(value = PageableConstants.MAX_PAGE_SIZE, message = "페이지 당 데이터 최댓값은 " + PageableConstants.MAX_PAGE_SIZE + "개 입니다.")
   private Integer pageSize; // 페이지 사이즈
 
-  @Schema(defaultValue = "created_date")
-  @Pattern(regexp = "^(createdDate|requestCount)$")
-  private String sortField; // 정렬 조건 (생성일, 매수)
+  private ApplicationFormSortField sortField; // 정렬 조건
 
-  @Schema(defaultValue = "DESC")
-  @Pattern(regexp = "^(ASC|DESC)$", message = "sortDirection에는 'ASC', 'DESC' 만 입력 가능합니다.")
-  private String sortDirection; // ASC, DESC
+  private Sort.Direction sortDirection; // 정렬 방향
 
-  // 기본값 할당 (1페이지 30개, 최신순)
+  // 기본값 할당 (1페이지 10개, 최신순)
   public ApplicationFormFilteredRequest() {
     this.pageNumber = 1;
     this.pageSize = PageableConstants.DEFAULT_PAGE_SIZE;
-    this.sortField = PageableConstants.DEFAULT_SORT_FIELD;
-    this.sortDirection = PageableConstants.DEFAULT_SORT_DIRECTION;
+    this.sortField = ApplicationFormSortField.CREATED_DATE;
+    this.sortDirection = Direction.DESC;
+  }
+
+  public Pageable toPageable() {
+    return PageableUtil.createPageable(
+        pageNumber,
+        pageSize,
+        PageableConstants.DEFAULT_PAGE_SIZE,
+        sortField,
+        sortDirection
+    );
   }
 }
