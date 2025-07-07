cc28de3d9c85753a22f016cbfaf02787379e30e6
Chuseok22 <bjh59629@naver.com>
2025-07-07 12:35:54 +0900

    filteredRequest DTO sortField enum 관리 및 QueryDSL 마이그레이션 : feat : PageableUtil 메서드 통일 https://github.com/Team-TicketMate/ticketmate-server/issues/393

--- src/main/java/com/ticketmate/backend/global/constant/PageableConstants.java
+++ src/main/java/com/ticketmate/backend/global/constant/PageableConstants.java
@@ -10,8 +10,6 @@ public final class PageableConstants {
   */
   public static final int MAX_PAGE_SIZE = 100;
   public static final int DEFAULT_PAGE_SIZE = 10;
-  public static final String DEFAULT_SORT_FIELD = "created_date";
-  public static final String DEFAULT_SORT_DIRECTION = "DESC";
 
     /*
   ======================================Custom Chatting Paging Data======================================
