6e07496ceb8c4b072fb8ce445ed3f3839f692a49
Chuseok22 <bjh59629@naver.com>
2025-07-07 12:46:58 +0900

    filteredRequest DTO sortField enum 관리 및 QueryDSL 마이그레이션 : refactor : from 메서드 @JsonCreator 어노테이션 추가 https://github.com/Team-TicketMate/ticketmate-server/issues/393

--- src/main/java/com/ticketmate/backend/domain/applicationform/domain/constant/ApplicationFormSortField.java
+++ src/main/java/com/ticketmate/backend/domain/applicationform/domain/constant/ApplicationFormSortField.java
@@ -1,5 +1,6 @@
 package com.ticketmate.backend.domain.applicationform.domain.constant;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.ticketmate.backend.global.constant.SortField;
 import com.ticketmate.backend.global.util.common.CommonUtil;
 import lombok.AllArgsConstructor;
@@ -24,6 +25,7 @@ public enum ApplicationFormSortField implements SortField {
    * 컨트롤러에서 들어온 {"sortField": "TICKET_OPEN_DATE"} 같은 문자열을 변환
    * 만약 {"sortField": "ticketOpenDate"}와 같이 카멜케이스로 들어와도 f.property와 비교하여 자동 매칭
    */
+  @JsonCreator
   public static ApplicationFormSortField from(String value) {
     return CommonUtil.stringToEnum(ApplicationFormSortField.class, value);
   }
