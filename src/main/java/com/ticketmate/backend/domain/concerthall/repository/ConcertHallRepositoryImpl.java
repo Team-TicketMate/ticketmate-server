9635a91a545aaad512216ecae884eaa3ea071e8f
Chuseok22 <bjh59629@naver.com>
2025-07-07 12:38:40 +0900

    filteredRequest DTO sortField enum 관리 및 QueryDSL 마이그레이션 : refactor : 공연장 필터링 조회 QueryDSL 마이그레이션 및 리팩토링 https://github.com/Team-TicketMate/ticketmate-server/issues/393

---
+++ src/main/java/com/ticketmate/backend/domain/concerthall/repository/ConcertHallRepositoryImpl.java
@@ -0,0 +1,70 @@
+package com.ticketmate.backend.domain.concerthall.repository;
+
+import com.querydsl.core.types.Projections;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQuery;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.ticketmate.backend.domain.concerthall.domain.constant.City;
+import com.ticketmate.backend.domain.concerthall.domain.dto.response.ConcertHallFilteredResponse;
+import com.ticketmate.backend.domain.concerthall.domain.entity.ConcertHall;
+import com.ticketmate.backend.domain.concerthall.domain.entity.QConcertHall;
+import com.ticketmate.backend.global.util.database.QueryDslUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class ConcertHallRepositoryImpl implements ConcertHallRepositoryCustom {
+
+  private final JPAQueryFactory queryFactory;
+
+  /**
+   * 공연장 필터링 조회
+   *
+   * @param concertHallName 공연장 이름
+   * @param city            지역
+   */
+  @Override
+  public Page<ConcertHallFilteredResponse> filteredConcertHall(
+      String concertHallName,
+      City city,
+      Pageable pageable) {
+
+    QConcertHall concertHall = QConcertHall.concertHall;
+
+    // 동적 WHERE 절 조합
+    BooleanExpression whereClause = QueryDslUtil.allOf(
+        QueryDslUtil.likeIgnoreCase(concertHall.concertHallName, concertHallName),
+        QueryDslUtil.eqIfNotNull(concertHall.city, city)
+    );
+
+    // 쿼리 작성
+    JPAQuery<ConcertHallFilteredResponse> contentQuery = queryFactory
+        .select(Projections.constructor(ConcertHallFilteredResponse.class,
+            concertHall.concertHallId,
+            concertHall.concertHallName,
+            concertHall.address,
+            concertHall.webSiteUrl
+        ))
+        .from(concertHall)
+        .where(whereClause);
+
+    // applySorting 동적 정렬 적용
+    QueryDslUtil.applySorting(
+        contentQuery,
+        pageable,
+        ConcertHall.class,
+        concertHall.getMetadata().getName()
+    );
+
+    // countQuery 생성
+    JPAQuery<Long> countQuery = queryFactory
+        .select(concertHall.count())
+        .from(concertHall)
+        .where(whereClause);
+
+    return QueryDslUtil.fetchPage(contentQuery, countQuery, pageable);
+  }
+}
