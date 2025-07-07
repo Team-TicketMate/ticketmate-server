dc355d7cc4ee2aba4ce6ae561f44645f51ce2b79
Chuseok22 <bjh59629@naver.com>
2025-07-07 12:34:25 +0900

    filteredRequest DTO sortField enum 관리 및 QueryDSL 마이그레이션 : feat : 필터링 조회 정렬 필드 인터페이스 추가 https://github.com/Team-TicketMate/ticketmate-server/issues/393

---
+++ src/main/java/com/ticketmate/backend/global/constant/SortField.java
@@ -0,0 +1,9 @@
+package com.ticketmate.backend.global.constant;
+
+/**
+ * 각 도메인이 제공한느 정렬 키 enum 이 구현해야 할 인터페이스
+ */
+public interface SortField {
+  // 실제 Sort.by() 에 넘길 프로퍼티 이름 (엔티티 속성명 ex.createdDate)
+  String getProperty();
+}
