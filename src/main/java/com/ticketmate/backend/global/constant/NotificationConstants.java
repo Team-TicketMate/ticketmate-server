515f6331a61ebfbc0b3ddcd4ab232079985fe6f3
Chuseok22 <bjh59629@naver.com>
2025-07-16 12:13:17 +0900

    Firebase 알림 로직 리팩토링 및 테스트 방안 마련 : feat : 알림 전송 로직 관련 리팩토링 (AdminService, ApplicationFormService, PortfolioService) https://github.com/Team-TicketMate/ticketmate-server/issues/419

--- src/main/java/com/ticketmate/backend/global/constant/NotificationConstants.java
+++ src/main/java/com/ticketmate/backend/global/constant/NotificationConstants.java
@@ -5,7 +5,15 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public final class NotificationConstants {
 
+  // FCM Token
+  public static final long FCM_TOKEN_TTL = 30 * 24 * 60L; // FCM 토큰 30일 후 파기
+
+  // Notification
+  public static final String NOTIFICATION_TTL_PREFIX = "ttl";
+  public static final String NOTIFICATION_TTL = "300";
+  public static final String NOTIFICATION_ICON_PATH = "resources/static/ticketmate-logo.png";
+
   // Notification Key
-  public static final String PLACE_HOLDER_NICK_NAME_KEY = "nickname";
-  public static final String PLACE_HOLDER_REJECT_OTHER_MEMO_KEY = "otherReject";
+  public static final String PLACEHOLDER_NICKNAME_KEY = "nickname";
+  public static final String PLACEHOLDER_REJECT_OTHER_MEMO_KEY = "otherReject";
 }
