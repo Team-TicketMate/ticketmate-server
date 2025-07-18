db4434c3baee72c6bde896e211915cd115f1939c
정우혁 <93671010+mr6208@users.noreply.github.com>
2025-07-18 18:33:56 +0900

    feat(chatting_message): 브로드캐스팅 및 채팅메시지 조회시 응답 DTO 필드 추가 #194

--- src/main/java/com/ticketmate/backend/domain/chat/domain/dto/response/ChatMessageResponse.java
+++ src/main/java/com/ticketmate/backend/domain/chat/domain/dto/response/ChatMessageResponse.java
@@ -2,12 +2,15 @@ package com.ticketmate.backend.domain.chat.domain.dto.response;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.ticketmate.backend.domain.chat.domain.constant.ChatMessageType;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 @Getter
@@ -18,17 +21,21 @@ public class ChatMessageResponse {
   private String messageId;
   private UUID senderId;
   private String senderNickname;
-  private String message;
+  private String message;  // 채팅 메시지 (텍스트)
   @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss", timezone = "Asia/Seoul")
   private LocalDateTime sendDate;
   @JsonIgnore
   private boolean read;  // 읽음 여부
   private String profileUrl;  // 프사
   private boolean mine;  // 메시지를 보낸 사람의 유무 (자신의 메시지이면 true/상대방의 메시지이면 false)
+  private ChatMessageType chatMessageType;  // 채팅메시지 종류 (이미지 or 텍스트)
+  @JsonInclude(JsonInclude.Include.NON_EMPTY)  // 사진이 없는 텍스트 메시지면 제외
+  private List<String> pictureMessageList;  // 사진 리스트
 
   @Builder
   public ChatMessageResponse(String chatRoomId, String messageId, UUID senderId, String senderNickname,
-                             String message, LocalDateTime sendDate, boolean read, String profileUrl, boolean mine) {
+                             String message, LocalDateTime sendDate, boolean read, String profileUrl,
+                             boolean mine, ChatMessageType chatMessageType, List<String> pictureMessageList) {
     this.chatRoomId = chatRoomId;
     this.messageId = messageId;
     this.senderId = senderId;
@@ -38,6 +45,8 @@ public class ChatMessageResponse {
     this.read = read;
     this.profileUrl = profileUrl;
     this.mine = mine;
+    this.chatMessageType = chatMessageType;
+    this.pictureMessageList = pictureMessageList;
   }
 
   @JsonProperty("isRead")
