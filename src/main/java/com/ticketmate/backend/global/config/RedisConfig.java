f911482f50801d9d9b54eed72461a7e8a97d7b87
장윤정 <wkddbs3935@naver.com>
2025-07-19 15:17:09 +0900

    feat(embedding-search): 캐싱용 config 설정

--- src/main/java/com/ticketmate/backend/global/config/RedisConfig.java
+++ src/main/java/com/ticketmate/backend/global/config/RedisConfig.java
@@ -1,10 +1,15 @@
 package com.ticketmate.backend.global.config;
 
+import java.time.Duration;
+import org.springframework.cache.CacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
@@ -22,6 +27,19 @@ public class RedisConfig {
     template.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
 
     return template;
+  }
+
+  @Bean
+  public CacheManager cacheManager(RedisConnectionFactory cf) {
+    RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
+        .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
+        .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()))
+        .entryTtl(Duration.ofMinutes(30L)); // 유효시간 30분
+
+    return RedisCacheManager.RedisCacheManagerBuilder
+        .fromConnectionFactory(cf)
+        .cacheDefaults(redisCacheConfiguration)
+        .build();
   }
 
 }
