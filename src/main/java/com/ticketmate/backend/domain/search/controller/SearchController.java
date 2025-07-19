68cd5544f2a6692917190b7ca7b4cc03215d0dad
장윤정 <wkddbs3935@naver.com>
2025-07-19 15:49:45 +0900

    docs(embedding-search): 검색 관련 swagger docs 적용

--- src/main/java/com/ticketmate/backend/domain/search/controller/SearchController.java
+++ src/main/java/com/ticketmate/backend/domain/search/controller/SearchController.java
@@ -3,6 +3,7 @@ package com.ticketmate.backend.domain.search.controller;
 import com.ticketmate.backend.domain.search.domain.dto.request.SearchRequest;
 import com.ticketmate.backend.domain.search.domain.dto.response.SearchResponse;
 import com.ticketmate.backend.domain.search.service.SearchService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -14,9 +15,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/search")
 @RequiredArgsConstructor
+@Tag(
+    name = "검색 API",
+    description = "공연/대리인 검색 관련 API 제공"
+)
 public class SearchController implements SearchControllerDocs {
   private final SearchService searchService;
 
+  @Override
   @GetMapping
   public ResponseEntity<SearchResponse<?>> search(@ModelAttribute @Valid SearchRequest request){
     return ResponseEntity.ok(searchService.search(request));
