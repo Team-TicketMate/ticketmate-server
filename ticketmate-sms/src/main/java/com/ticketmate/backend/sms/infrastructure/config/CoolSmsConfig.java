430c3d38b1199c45d66299301d3f3856b39adc0c
Chuseok22 <bjh59629@naver.com>
2025-07-30 10:23:40 +0900

    도메인 기반 멀티 모듈 구조 분리 및 계층화 : feat : [sms 모듈] CoolSms 관련 config, properties 클래스 이전 https://github.com/Team-TicketMate/ticketmate-server/issues/444

--- ticketmate-sms/src/main/java/com/ticketmate/backend/sms/infrastructure/config/CoolSmsConfig.java
+++ ticketmate-sms/src/main/java/com/ticketmate/backend/sms/infrastructure/config/CoolSmsConfig.java
@@ -1,14 +1,16 @@
-package com.ticketmate.backend.global.config.beans;
+package com.ticketmate.backend.sms.infrastructure.config;
 
-import com.ticketmate.backend.global.config.properties.CoolSmsProperties;
+import com.ticketmate.backend.sms.infrastructure.properties.CoolSmsProperties;
 import lombok.RequiredArgsConstructor;
 import net.nurigo.sdk.NurigoApp;
 import net.nurigo.sdk.message.service.DefaultMessageService;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @RequiredArgsConstructor
+@EnableConfigurationProperties(CoolSmsProperties.class)
 public class CoolSmsConfig {
 
   private final CoolSmsProperties properties;
