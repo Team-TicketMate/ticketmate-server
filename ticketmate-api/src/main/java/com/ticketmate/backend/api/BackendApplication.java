198d9ceb29ad6747978390a9b2a442b9a3a33fa8
Chuseok22 <bjh59629@naver.com>
2025-07-30 10:03:02 +0900

    도메인 기반 멀티 모듈 구조 분리 및 계층화 : feat : [api 모듈] SpringApplication.java 메인 실행 클래스 이동 https://github.com/Team-TicketMate/ticketmate-server/issues/444

--- ticketmate-api/src/main/java/com/ticketmate/backend/api/BackendApplication.java
+++ ticketmate-api/src/main/java/com/ticketmate/backend/api/BackendApplication.java
@@ -1,4 +1,4 @@
-package com.ticketmate.backend;
+package com.ticketmate.backend.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
