36f2b5547ee2dd6c94d923cd62f15caa3951edcc
Yooonjeong <wkddbs3935@naver.com>
2025-08-18 11:25:34 +0900

    feat(recent-searches): SearchProperties를 이용한 설정 관리

---
+++ ticketmate-search/src/main/java/com/ticketmate/backend/search/infrastructure/config/SearchConfig.java
@@ -0,0 +1,10 @@
+package com.ticketmate.backend.search.infrastructure.config;
+
+import com.ticketmate.backend.search.infrastructure.properties.SearchProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableConfigurationProperties(SearchProperties.class)
+public class SearchConfig {
+}
