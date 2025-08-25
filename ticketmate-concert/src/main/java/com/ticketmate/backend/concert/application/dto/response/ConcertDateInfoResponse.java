70cceba3b8da8bfdcde4283b09b25cf6a864c024
Chuseok22 <bjh59629@naver.com>
2025-08-25 14:48:48 +0900

    LocalDateTime 직렬화역직렬화 중앙 관리 : feat : ConcertDateInfoResponse class -> record 변경 https://github.com/Team-TicketMate/ticketmate-server/issues/482

--- ticketmate-concert/src/main/java/com/ticketmate/backend/concert/application/dto/response/ConcertDateInfoResponse.java
+++ ticketmate-concert/src/main/java/com/ticketmate/backend/concert/application/dto/response/ConcertDateInfoResponse.java
@@ -2,25 +2,13 @@ package com.ticketmate.backend.concert.application.dto.response;
 
 import com.ticketmate.backend.concert.infrastructure.entity.ConcertDate;
 import java.time.LocalDateTime;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
 
-@ToString
-@AllArgsConstructor
-@NoArgsConstructor
-@Getter
-@Setter
 @Builder
-public class ConcertDateInfoResponse {
-
-  private LocalDateTime performanceDate; // 공연일자
-
-  private int session; // 회차
-
+public record ConcertDateInfoResponse(
+    LocalDateTime performanceDate, // 공연일자
+    int session // 회차
+) {
   public static ConcertDateInfoResponse from(ConcertDate concertDate) {
     return ConcertDateInfoResponse.builder()
         .performanceDate(concertDate.getPerformanceDate())
