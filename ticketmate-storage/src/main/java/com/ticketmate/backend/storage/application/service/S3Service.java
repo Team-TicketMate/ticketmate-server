3b38f806e79179bbcdb4c49968af869b2d4d54f9
Chuseok22 <bjh59629@naver.com>
2025-08-29 17:14:12 +0900

    파일 업로드 삭제 스펙 개선 URL 의존 제거 및 storageKeyfileId 기반 : feat : [storage 모듈] 파일 업로드 시 FileMetadata 반환 및 DB에 storedPath 저장 https://github.com/Team-TicketMate/ticketmate-server/issues/492

--- ticketmate-storage/src/main/java/com/ticketmate/backend/storage/application/service/S3Service.java
+++ ticketmate-storage/src/main/java/com/ticketmate/backend/storage/application/service/S3Service.java
@@ -10,6 +10,7 @@ import com.ticketmate.backend.common.application.exception.ErrorCode;
 import com.ticketmate.backend.common.core.util.CommonUtil;
 import com.ticketmate.backend.storage.core.constant.FileExtension;
 import com.ticketmate.backend.storage.core.constant.UploadType;
+import com.ticketmate.backend.storage.core.model.FileMetadata;
 import com.ticketmate.backend.storage.core.service.StorageService;
 import com.ticketmate.backend.storage.infrastructure.properties.S3Properties;
 import com.ticketmate.backend.storage.infrastructure.util.FileUtil;
@@ -23,6 +24,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 @Primary
@@ -37,35 +39,9 @@ public class S3Service implements StorageService {
   private final S3Properties s3Properties;
   private final Clock clock;
 
-  /**
-   * 파일 검증 및 원본 파일명 반환
-   *
-   * @param file 요청된 MultipartFile
-   * @return 원본 파일명
-   */
-  private static String validateAndExtractFilename(MultipartFile file) {
-    // 파일 검증
-    if (FileUtil.isNullOrEmpty(file)) {
-      throw new CustomException(ErrorCode.INVALID_FILE_REQUEST);
-    }
-
-    // 원본 파일 명 검증
-    String originalFilename = file.getOriginalFilename();
-    if (CommonUtil.nvl(originalFilename, "").isEmpty()) {
-      throw new CustomException(ErrorCode.INVALID_FILE_REQUEST);
-    }
-    return originalFilename;
-  }
-
-  /**
-   * S3 파일 업로드 후 파일 URL 반환
-   *
-   * @param file       업로드할 MultipartFile
-   * @param uploadType 업로드할 파일의 도메인 구분
-   * @return 업로드된 파일에 접근 가능한 URL
-   */
   @Override
-  public String uploadFile(MultipartFile file, UploadType uploadType) {
+  @Transactional
+  public FileMetadata uploadFile(MultipartFile file, UploadType uploadType) {
 
     // 파일 검증 및 원본 파일명 추출
     String originalFilename = validateAndExtractFilename(file);
@@ -77,8 +53,8 @@ public class S3Service implements StorageService {
     String prefix = determinePrefix(uploadType);
 
     // 중복이 되지 않는 고유한 파일이름 생성
-    String filename = generateFilename(originalFilename, prefix);
-    log.debug("생성된 파일명: {}", filename);
+    String storedPath = generateStoredPath(originalFilename, prefix);
+    log.debug("생성된 파일명: {}", storedPath);
 
     ObjectMetadata metadata = new ObjectMetadata();
     metadata.setContentLength(file.getSize());
@@ -86,93 +62,103 @@ public class S3Service implements StorageService {
 
     // S3 파일 업로드
     try (InputStream inputStream = file.getInputStream()) {
-      amazonS3.putObject(s3Properties.s3().bucket(), filename, inputStream, metadata);
-      log.debug("S3 파일 업로드 성공: {}", filename);
+      amazonS3.putObject(s3Properties.s3().bucket(), storedPath, inputStream, metadata);
+      log.debug("S3 파일 업로드 성공: {}", storedPath);
     } catch (AmazonServiceException ase) {
-      log.error("AmazonServiceException - S3 파일 업로드 실패. 버킷: {}, 파일명: {}, 에러: {}", s3Properties.s3().bucket(), filename, ase.getMessage());
+      log.error("AmazonServiceException - S3 파일 업로드 실패. 버킷: {}, 파일명: {}, 에러: {}", s3Properties.s3().bucket(), storedPath, ase.getMessage());
       throw new CustomException(ErrorCode.S3_UPLOAD_AMAZON_SERVICE_ERROR);
     } catch (AmazonClientException ace) {
-      log.error("AmazonClientException - S3 클라이언트 에러 발생. 버킷: {}, 파일명: {}, 에러: {}", s3Properties.s3().bucket(), filename, ace.getMessage());
+      log.error("AmazonClientException - S3 클라이언트 에러 발생. 버킷: {}, 파일명: {}, 에러: {}", s3Properties.s3().bucket(), storedPath, ace.getMessage());
       throw new CustomException(ErrorCode.S3_UPLOAD_AMAZON_CLIENT_ERROR);
     } catch (IOException ioe) {
-      log.error("IOException - 파일 스트림 처리 중 에러 발생. 원본 파일명: {}, 파일명: {} 에러: {}", originalFilename, filename, ioe.getMessage());
+      log.error("IOException - 파일 스트림 처리 중 에러 발생. 원본 파일명: {}, 파일명: {} 에러: {}", originalFilename, storedPath, ioe.getMessage());
       throw new CustomException(ErrorCode.S3_UPLOAD_ERROR);
     }
 
-    // 파일 URL 생성 및 반환
-    return generateFilePath(filename);
+    // FileMetadata 반환
+    return new FileMetadata(originalFilename, storedPath, fileExtension, file.getSize());
   }
 
-  /**
-   * S3에서 파일 삭제
-   *
-   * @param fileUrl 삭제할 파일의 URL
-   */
   @Override
-  public void deleteFile(String fileUrl) {
-    if (CommonUtil.nvl(fileUrl, "").isEmpty()) {
-      log.warn("삭제할 파일 URL이 없습니다.");
+  public String generatePublicUrl(String storedPath) {
+    if (storedPath.startsWith("https://picsum.photos/")) {
+      return storedPath;
+    } // TODO: Mock 데이터 사진은 "https://picsum.photos/..." 고정이므로 Mock 데이터는 storedPath를 저장하는 것이 아닌 publicUrl 전체 저장 (개발 후 삭제 필요)
+    return s3Properties.s3().domain() + storedPath; // 예: "https://domain.com/prefix/yyyyMMdd-UUID-파일명.jpg"
+  }
+
+
+  @Override
+  public String extractStoredPathFromPublicUrl(String publicUrl) {
+    String storedPath = "";
+    if (publicUrl.startsWith(s3Properties.s3().domain())) {
+      storedPath = publicUrl.substring(s3Properties.s3().domain().length());
+    }
+    if (CommonUtil.nvl(storedPath, "").isEmpty()) {
+      log.error("파일 저장 경로 추출 실패: {}", publicUrl);
+      throw new CustomException(ErrorCode.INVALID_PUBLIC_URL);
+    }
+    log.debug("파일 저장 경로 추출 성공: {}", storedPath);
+    return storedPath;
+  }
+
+  @Override
+  @Transactional
+  public void deleteFile(String storedPath) {
+    if (CommonUtil.nvl(storedPath, "").isEmpty()) {
+      log.warn("요청된 파일 경로가 없습니다.");
       return;
     }
 
-    // URL에서 도메인 제거 (filename만 추출)
-    String filename = extractFilenameFromFilepath(fileUrl);
-
     // S3 파일 삭제
     try {
-      amazonS3.deleteObject(new DeleteObjectRequest(s3Properties.s3().bucket(), filename));
-      log.debug("S3 파일 삭제 성공: {}", filename);
+      amazonS3.deleteObject(new DeleteObjectRequest(s3Properties.s3().bucket(), storedPath));
+      log.debug("S3 파일 삭제 성공: {}", storedPath);
     } catch (AmazonServiceException ase) {
-      log.error("AmazonServiceException - S3 파일 삭제 실패. 버킷: {}, 파일명: {}, 에러: {}", s3Properties.s3().bucket(), filename, ase.getMessage());
+      log.error("AmazonServiceException - S3 파일 삭제 실패. 버킷: {}, 파일명: {}, 에러: {}", s3Properties.s3().bucket(), storedPath, ase.getMessage());
       throw new CustomException(ErrorCode.S3_DELETE_AMAZON_SERVICE_ERROR);
     } catch (AmazonClientException ace) {
-      log.error("AmazonClientException - S3 파일 삭제 실패. 버킷: {}, 파일명: {}, 에러: {}", s3Properties.s3().bucket(), filename, ace.getMessage());
+      log.error("AmazonClientException - S3 파일 삭제 실패. 버킷: {}, 파일명: {}, 에러: {}", s3Properties.s3().bucket(), storedPath, ace.getMessage());
       throw new CustomException(ErrorCode.S3_DELETE_AMAZON_CLIENT_ERROR);
     } catch (Exception e) {
-      log.error("S3 파일 삭제 실패. 버킷: {}, 파일명: {}, 에러: {}", s3Properties.s3().bucket(), filename, e.getMessage());
+      log.error("S3 파일 삭제 실패. 버킷: {}, 파일명: {}, 에러: {}", s3Properties.s3().bucket(), storedPath, e.getMessage());
       throw new CustomException(ErrorCode.S3_DELETE_ERROR);
     }
   }
 
   /**
-   * 파일 명 생성 (prefix/yyyyMMdd-UUID-파일명.jpg)
+   * 파일 검증 및 원본 파일명 반환
+   *
+   * @param file 요청된 MultipartFile
+   * @return 원본 파일명
+   */
+  private static String validateAndExtractFilename(MultipartFile file) {
+    // 파일 검증
+    if (FileUtil.isNullOrEmpty(file)) {
+      log.error("파일이 비어있거나 존재하지 않습니다.");
+      throw new CustomException(ErrorCode.INVALID_FILE_REQUEST);
+    }
+
+    // 원본 파일 명 검증
+    String originalFilename = file.getOriginalFilename();
+    if (CommonUtil.nvl(originalFilename, "").isEmpty()) {
+      log.error("원본 파일명이 비어있거나 존재하지 않습니다.");
+      throw new CustomException(ErrorCode.INVALID_FILE_REQUEST);
+    }
+    return originalFilename;
+  }
+
+  /**
+   * 저장 경로(storedPath) 생성 (prefix/yyyyMMdd-UUID-파일명.jpg)
    *
    * @param originalFilename 원본 파일명
-   * @return UUID-originalFilename: 가공된 파일명
+   * @return 저장 경로(storedPath) (prefix/yyyyMMdd-UUID-파일명.jpg)
    */
-  private String generateFilename(String originalFilename, String prefix) {
+  private String generateStoredPath(String originalFilename, String prefix) {
     String datePart = LocalDateTime.now(clock).format(DATE_TIME_FORMATTER);
     // 파일명에서 경로 구분자와 특수 문자 제거
     String sanitizedFilename = originalFilename.replaceAll("[/\\\\:*?\"<>|]", "_");
     return String.format("%s/%s-%s-%s", prefix, datePart, UUID.randomUUID(), sanitizedFilename);
-  }
-
-  /**
-   * 파일 URL 생성
-   *
-   * @param filename 파일명 (prefix/yyyyMMdd-UUID-파일명.jpg)
-   * @return 파일 URL
-   */
-  private String generateFilePath(String filename) {
-    return s3Properties.s3().domain() + filename; // 예: "member/20250605-a1b2c3-원본이미지.jpg"
-  }
-
-  /**
-   * 파일 URL에서 파일명 추출
-   *
-   * @param filePath 파일 URL
-   * @return filename
-   */
-  private String extractFilenameFromFilepath(String filePath) {
-    String filename = filePath;
-    if (filePath.startsWith(s3Properties.s3().domain())) {
-      filename = filePath.substring(s3Properties.s3().domain().length());
-    }
-    if (CommonUtil.nvl(filename, "").isEmpty()) {
-      log.error("파일명 추출 실패: {}", filePath);
-      throw new CustomException(ErrorCode.INVALID_FILE_PATH);
-    }
-    return filename;
   }
 
   /**
@@ -191,11 +177,11 @@ public class S3Service implements StorageService {
   /**
    * S3에 파일은 업로드 되어있지만 서버 내부적으로 오류 발생시 이미지 롤백(삭제)
    */
-  public void safeDeleteFile(String path) {
+  public void safeDeleteFile(String storedPath) {
     try {
-      deleteFile(path);
+      deleteFile(storedPath);
     } catch (Exception ex) {
-      log.warn("S3 파일 롤백 실패: {}", path, ex);
+      log.warn("S3 파일 롤백 실패: {}", storedPath, ex);
     }
   }
 }
