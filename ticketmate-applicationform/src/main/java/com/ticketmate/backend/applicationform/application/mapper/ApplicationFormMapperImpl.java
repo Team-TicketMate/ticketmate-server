b03f76a2709e8eccfeb5235ae0a83e954fb6d41d
Chuseok22 <bjh59629@naver.com>
2025-08-29 20:26:13 +0900

    파일 업로드 삭제 스펙 개선 URL 의존 제거 및 storageKeyfileId 기반 : feat : [applicationform 모듈] 신청서 관련 mapper 및 repository 쿼리문 수정 https://github.com/Team-TicketMate/ticketmate-server/issues/492

---
+++ ticketmate-applicationform/src/main/java/com/ticketmate/backend/applicationform/application/mapper/ApplicationFormMapperImpl.java
@@ -0,0 +1,78 @@
+package com.ticketmate.backend.applicationform.application.mapper;
+
+import com.ticketmate.backend.applicationform.application.dto.request.HopeAreaRequest;
+import com.ticketmate.backend.applicationform.application.dto.response.ApplicationFormDetailResponse;
+import com.ticketmate.backend.applicationform.application.dto.response.ApplicationFormFilteredResponse;
+import com.ticketmate.backend.applicationform.application.dto.response.HopeAreaResponse;
+import com.ticketmate.backend.applicationform.application.dto.response.RejectionReasonResponse;
+import com.ticketmate.backend.applicationform.application.dto.view.ApplicationFormFilteredInfo;
+import com.ticketmate.backend.applicationform.infrastructure.entity.ApplicationFormDetail;
+import com.ticketmate.backend.applicationform.infrastructure.entity.HopeArea;
+import com.ticketmate.backend.applicationform.infrastructure.entity.RejectionReason;
+import com.ticketmate.backend.storage.core.service.StorageService;
+import java.util.List;
+import java.util.stream.Collectors;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class ApplicationFormMapperImpl implements ApplicationFormMapper {
+
+  private final ApplicationFormMapStruct mapStruct;
+  private final StorageService storageService;
+
+  @Override
+  public List<HopeArea> toHopeAreaList(List<HopeAreaRequest> hopeAreaRequestList) {
+    return mapStruct.toHopeAreaList(hopeAreaRequestList);
+  }
+
+  @Override
+  public HopeAreaResponse toHopeAreaResponse(HopeArea hopeArea) {
+    return mapStruct.toHopeAreaResponse(hopeArea);
+  }
+
+  @Override
+  public List<HopeAreaResponse> toHopeAreaResponseList(List<HopeArea> hopeAreaList) {
+    return mapStruct.toHopeAreaResponseList(hopeAreaList);
+  }
+
+  @Override
+  public ApplicationFormDetailResponse toApplicationFormDetailResponse(ApplicationFormDetail applicationFormDetail) {
+    return mapStruct.toApplicationFormDetailResponse(applicationFormDetail);
+  }
+
+  @Override
+  public List<ApplicationFormDetailResponse> toApplicationFormDetailResponseList(List<ApplicationFormDetail> applicationFormDetailList) {
+    return mapStruct.toApplicationFormDetailResponseList(applicationFormDetailList);
+  }
+
+  @Override
+  public ApplicationFormFilteredResponse toApplicationFormFilteredResponse(ApplicationFormFilteredInfo info) {
+    return new ApplicationFormFilteredResponse(
+        info.applicationFormId(),
+        info.concertName(),
+        storageService.generatePublicUrl(info.concertThumbnailImgStoredPath()),
+        info.agentNickname(),
+        info.clientNickname(),
+        info.submittedDate(),
+        info.applicationFormStatus(),
+        info.ticketOpenType()
+    );
+  }
+
+  @Override
+  public Page<ApplicationFormFilteredResponse> toApplicationFormFilteredResponsePage(Page<ApplicationFormFilteredInfo> infoPage) {
+    List<ApplicationFormFilteredResponse> content = infoPage.getContent().stream()
+        .map(this::toApplicationFormFilteredResponse)
+        .collect(Collectors.toList());
+    return new PageImpl<>(content, infoPage.getPageable(), infoPage.getTotalElements());
+  }
+
+  @Override
+  public RejectionReasonResponse toRejectionReasonResponse(RejectionReason rejectionReason) {
+    return mapStruct.toRejectionReasonResponse(rejectionReason);
+  }
+}
