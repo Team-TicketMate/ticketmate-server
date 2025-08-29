9b701882f434f2fbf9457e52980f7dbfc6588075
Chuseok22 <bjh59629@naver.com>
2025-08-29 20:31:06 +0900

    파일 업로드 삭제 스펙 개선 URL 의존 제거 및 storageKeyfileId 기반 : feat : [search 모듈] mapper 추가 및 SearchService 내부 로직 수정 https://github.com/Team-TicketMate/ticketmate-server/issues/492

---
+++ ticketmate-search/src/main/java/com/ticketmate/backend/search/application/mapper/SearchMapperImpl.java
@@ -0,0 +1,43 @@
+package com.ticketmate.backend.search.application.mapper;
+
+import com.ticketmate.backend.search.application.dto.response.AgentSearchResponse;
+import com.ticketmate.backend.search.application.dto.response.ConcertSearchResponse;
+import com.ticketmate.backend.search.application.dto.view.AgentSearchInfo;
+import com.ticketmate.backend.search.application.dto.view.ConcertSearchInfo;
+import com.ticketmate.backend.storage.core.service.StorageService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class SearchMapperImpl implements SearchMapper{
+
+  private final StorageService storageService;
+
+  @Override
+  public ConcertSearchResponse toConcertSearchResponse(ConcertSearchInfo info) {
+    return new ConcertSearchResponse(
+        info.concertId(),
+        info.concertName(),
+        info.concertHallName(),
+        info.ticketPreOpenDate(),
+        info.ticketGeneralOpenDate(),
+        info.startDate(),
+        info.endDate(),
+        storageService.generatePublicUrl(info.concertThumbnailStoredPath()),
+        info.score()
+    );
+  }
+
+  @Override
+  public AgentSearchResponse toAgentSearchResponse(AgentSearchInfo info) {
+    return new AgentSearchResponse(
+        info.agentId(),
+        info.nickname(),
+        storageService.generatePublicUrl(info.profileImgStoredPath()),
+        info.introduction(),
+        info.averageRating(),
+        info.reviewCount()
+    );
+  }
+}
