eef7362254f1ac1ca00d35c2ac24e73485d35d00
Chuseok22 <bjh59629@naver.com>
2025-09-01 09:03:09 +0900

    파일 업로드 삭제 스펙 개선 URL 의존 제거 및 storageKeyfileId 기반 : feat : [admin 모듈] PortfolioAdminMapperImpl 내부 storedPath NPE 안전성 강화 https://github.com/Team-TicketMate/ticketmate-server/issues/492

--- ticketmate-admin/src/main/java/com/ticketmate/backend/admin/portfolio/application/mapper/PortfolioAdminMapperImpl.java
+++ ticketmate-admin/src/main/java/com/ticketmate/backend/admin/portfolio/application/mapper/PortfolioAdminMapperImpl.java
@@ -2,7 +2,9 @@ package com.ticketmate.backend.admin.portfolio.application.mapper;
 
 import com.ticketmate.backend.admin.portfolio.application.dto.response.PortfolioAdminResponse;
 import com.ticketmate.backend.admin.portfolio.application.dto.view.PortfolioAdminInfo;
+import com.ticketmate.backend.common.core.util.CommonUtil;
 import com.ticketmate.backend.storage.core.service.StorageService;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
@@ -10,16 +12,19 @@ import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
-public class PortfolioAdminMapperImpl implements PortfolioAdminMapper{
+public class PortfolioAdminMapperImpl implements PortfolioAdminMapper {
 
   private final PortfolioAdminMapStruct mapStruct;
   private final StorageService storageService;
 
   @Override
   public PortfolioAdminResponse toPortfolioAdminResponse(PortfolioAdminInfo info) {
-    List<String> portfolioImgPublicUrlList = info.portfolioImgStoredPathList().stream()
-        .map(storageService::generatePublicUrl)
-        .collect(Collectors.toList());
+    List<String> portfolioImgPublicUrlList = new ArrayList<>();
+    if (!CommonUtil.nullOrEmpty(info.portfolioImgStoredPathList())) {
+      portfolioImgPublicUrlList = info.portfolioImgStoredPathList().stream()
+          .map(storageService::generatePublicUrl)
+          .collect(Collectors.toList());
+    }
 
     return new PortfolioAdminResponse(
         info.portfolioId(),
