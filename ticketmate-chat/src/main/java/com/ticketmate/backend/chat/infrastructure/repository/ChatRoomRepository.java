6a50598201ea552147cfb1a0b38df3c6f69eea85
정우혁 <93671010+mr6208@users.noreply.github.com>
2025-09-01 15:16:22 +0900

    20250829 #194 진행취소 기능구현  (#493) (#498)

--- ticketmate-chat/src/main/java/com/ticketmate/backend/chat/infrastructure/repository/ChatRoomRepository.java
+++ ticketmate-chat/src/main/java/com/ticketmate/backend/chat/infrastructure/repository/ChatRoomRepository.java
@@ -2,6 +2,7 @@ package com.ticketmate.backend.chat.infrastructure.repository;
 
 import com.ticketmate.backend.chat.infrastructure.entity.ChatRoom;
 import com.ticketmate.backend.concert.core.constant.TicketOpenType;
+import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
@@ -10,6 +11,13 @@ public interface ChatRoomRepository extends MongoRepository<ChatRoom, String>, C
   boolean existsByAgentMemberIdAndClientMemberIdAndConcertIdAndTicketOpenType(
       UUID agentId,
       UUID clientId,
+      UUID concertId,
+      TicketOpenType ticketOpenType
+  );
+
+  Optional<ChatRoom> findByAgentMemberIdAndClientMemberIdAndConcertIdAndTicketOpenType(
+      UUID agentMemberId,
+      UUID clientMemberId,
       UUID concertId,
       TicketOpenType ticketOpenType
   );
