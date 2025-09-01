c3de0e18d5b71fd15966eedc5703cd0dfb0fa6bd
Chuseok22 <bjh59629@naver.com>
2025-09-01 09:21:10 +0900

    파일 업로드 삭제 스펙 개선 URL 의존 제거 및 storageKeyfileId 기반 : feat : [application 모듈] ApplicationFormDetailRequest DTO 내부 @ToString 어노테이션 제거 https://github.com/Team-TicketMate/ticketmate-server/issues/492

--- ticketmate-applicationform/src/main/java/com/ticketmate/backend/applicationform/application/dto/request/ApplicationFormDetailRequest.java
+++ ticketmate-applicationform/src/main/java/com/ticketmate/backend/applicationform/application/dto/request/ApplicationFormDetailRequest.java
@@ -13,13 +13,11 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import lombok.ToString;
 
-@ToString
-@NoArgsConstructor
-@AllArgsConstructor
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class ApplicationFormDetailRequest {
 
