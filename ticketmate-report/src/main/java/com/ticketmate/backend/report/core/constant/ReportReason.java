749d969c06429e169035b260c53bdf3b3aac0098
Yooonjeong <wkddbs3935@naver.com>
2025-09-01 20:36:33 +0900

    feat(user-report): Report 관련 Enum 정의

---
+++ ticketmate-report/src/main/java/com/ticketmate/backend/report/core/constant/ReportReason.java
@@ -0,0 +1,31 @@
+package com.ticketmate.backend.report.core.constant;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.ticketmate.backend.common.application.exception.CustomException;
+import com.ticketmate.backend.common.application.exception.ErrorCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum ReportReason {
+  INAPPROPRIATE_LANGUAGE("불건전한 언행 사용"),
+  FAKE_LISTING("허위매물"),
+  SPAM("스팸");
+
+  private final String description;
+
+  public String getCode() { return name(); }
+
+  @JsonCreator
+  public static ReportReason from(String code){
+    for (ReportReason reportReason : ReportReason.values()) {
+      if(reportReason.getCode().equals(code)){
+        return reportReason;
+      }
+    }
+    throw new CustomException(ErrorCode.INVALID_REPORT_REASON);
+  }
+
+  // TODO: 신고 사유 재정의 필요
+}
