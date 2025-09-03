1dcd19d8e55f9a9c5d45951be790626c01524846
Chuseok22 <bjh59629@naver.com>
2025-09-03 13:46:13 +0900

    Flyway 도입 및 스키마 검증 CI 기본 세팅 : feat : active profile: schema-verify 수정 및 컨텍스트 실행 후 ddl 비교 https://github.com/Team-TicketMate/ticketmate-server/issues/505

---
+++ ticketmate-api/src/test/java/com/ticketmate/backend/api/SchemaValidationTest.java
@@ -0,0 +1,15 @@
+package com.ticketmate.backend.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("schema-verify")
+public class SchemaValidationTest {
+
+  @Test
+  void contextLoad() {
+
+  }
+}
