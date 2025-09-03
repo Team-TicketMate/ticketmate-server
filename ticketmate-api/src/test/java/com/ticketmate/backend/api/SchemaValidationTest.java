75a780f3385c7961dc4fec9c3775e36edf5ba6b0
Chuseok22 <bjh59629@naver.com>
2025-09-03 15:46:04 +0900

    Revert "Flyway 도입 및 스키마 검증 CI 기본 세팅 : feat : JPA를 제외한 불필요한 Bean 의존성 제거 https://github.com/Team-TicketMate/ticketmate-server/issues/505"
    This reverts commit 3351e7422a57e9c91b24287ae5fbebdabd3aafba.

--- ticketmate-api/src/test/java/com/ticketmate/backend/api/SchemaValidationTest.java
+++ ticketmate-api/src/test/java/com/ticketmate/backend/api/SchemaValidationTest.java
@@ -2,8 +2,10 @@ package com.ticketmate.backend.api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest(classes = SchemaVerifyApplication.class)
+@SpringBootTest
+@ActiveProfiles("schema-verify")
 public class SchemaValidationTest {
 
   @Test
