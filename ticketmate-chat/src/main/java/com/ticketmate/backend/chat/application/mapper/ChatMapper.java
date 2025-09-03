98d6632b9176a4516105f15024efe3ae73d81433
Chuseok22 <bjh59629@naver.com>
2025-09-03 11:08:24 +0900

    채팅 메시지에서 프로필 이미지 저장 경로를 공개 URL로 변환하는 로직 추가 : feat : ChatMapper 변경 및 mapper 관련 로직 리팩토링 https://github.com/Team-TicketMate/ticketmate-server/issues/497

--- ticketmate-chat/src/main/java/com/ticketmate/backend/chat/application/mapper/ChatMapper.java
+++ ticketmate-chat/src/main/java/com/ticketmate/backend/chat/application/mapper/ChatMapper.java
@@ -1,20 +1,23 @@
 package com.ticketmate.backend.chat.application.mapper;
 
+import com.ticketmate.backend.applicationform.infrastructure.entity.ApplicationForm;
 import com.ticketmate.backend.chat.application.dto.response.ChatMessageResponse;
+import com.ticketmate.backend.chat.application.dto.response.ChatRoomResponse;
 import com.ticketmate.backend.chat.infrastructure.entity.ChatMessage;
+import com.ticketmate.backend.chat.infrastructure.entity.ChatRoom;
+import com.ticketmate.backend.member.infrastructure.entity.Member;
+import java.util.Map;
 import java.util.UUID;
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 
-@Mapper(componentModel = "spring")
 public interface ChatMapper {
 
-  /**
-   * ChatMessage(Mongo) → ChatMessageResponse(DTO)
-   */
-  @Mapping(source = "message.chatMessageId", target = "messageId")
-  @Mapping(source = "message.senderNickName", target = "senderNickname")
-  @Mapping(source = "message.senderProfileUrl", target = "profileUrl")
-  @Mapping(target = "mine", expression = "java(message.getSenderId().equals(currentMemberId))")
   ChatMessageResponse toChatMessageResponse(ChatMessage message, UUID currentMemberId);
+
+  ChatRoomResponse toChatRoomResponse(
+      ChatRoom chatRoom,
+      Member member,
+      Map<UUID, ApplicationForm> applicationFormMap,
+      Map<UUID, Member> mmemberMap,
+      int unRead
+  );
 }
