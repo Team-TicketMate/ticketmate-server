7e4623d11b0e43f19bf8f28bee4ef811ca0144c4
Chuseok22 <bjh59629@naver.com>
2025-09-08 22:46:46 +0900

    PortfolioStatusUpdateRequest 역직렬화 오류 및 Docs 누락 수정 : feat : PortfolioStatusUpdateRequest 역직렬화 문제 해결 (생성자 추가) https://github.com/Team-TicketMate/ticketmate-server/issues/512

--- ticketmate-admin/src/main/java/com/ticketmate/backend/admin/portfolio/application/dto/request/PortfolioStatusUpdateRequest.java
+++ ticketmate-admin/src/main/java/com/ticketmate/backend/admin/portfolio/application/dto/request/PortfolioStatusUpdateRequest.java
@@ -2,13 +2,17 @@ package com.ticketmate.backend.admin.portfolio.application.dto.request;
 
 import com.ticketmate.backend.portfolio.core.constant.PortfolioStatus;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class PortfolioStatusUpdateRequest {
 
   @NotNull(message = "변경하려는 포트폴리오 상태를 입력하세요.")
