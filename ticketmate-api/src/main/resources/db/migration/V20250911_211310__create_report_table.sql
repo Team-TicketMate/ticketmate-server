6b83b601c72bc0aa90f5e0dd51e87a046a5a1e36
Yooonjeong <wkddbs3935@naver.com>
2025-09-12 00:29:10 +0900

    refactor(user-report): CONSTRAINT 해시값으로 변경

--- ticketmate-api/src/main/resources/db/migration/V20250911_211310__create_report_table.sql
+++ ticketmate-api/src/main/resources/db/migration/V20250911_211310__create_report_table.sql
@@ -3,9 +3,9 @@ CREATE TABLE public.report
 (
     report_id               UUID           NOT NULL PRIMARY KEY,
     reporter_member_id      UUID
-        CONSTRAINT fk_report_on_reporter REFERENCES member,
+        CONSTRAINT FK4u358afc7n9mo843563w364lp REFERENCES member,
     reported_user_member_id UUID
-        CONSTRAINT fk_report_on_reported_user REFERENCES member,
+        CONSTRAINT FKcdhkgbi9qbj1gnpqg2o1f3ido REFERENCES member,
     reason           VARCHAR(255)   NOT NULL
         CONSTRAINT report_reason_check
             CHECK ((reason)::TEXT = ANY ((ARRAY ['INAPPROPRIATE_LANGUAGE'::CHARACTER VARYING, 'FAKE_LISTING'::CHARACTER VARYING, 'SPAM'::CHARACTER VARYING])::TEXT[])),
