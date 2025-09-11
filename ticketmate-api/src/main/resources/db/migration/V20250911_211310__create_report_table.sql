7270ea2271736c2926416d528a0fc77229163a17
Yooonjeong <wkddbs3935@naver.com>
2025-09-11 22:27:18 +0900

    feat(user-report): Report 테이블 추가에 따른 flyway 코드 작성

---
+++ ticketmate-api/src/main/resources/db/migration/V20250911_211310__create_report_table.sql
@@ -0,0 +1,18 @@
+-- Report(신고) 테이블 생성
+CREATE TABLE public.report
+(
+    report_id        UUID           NOT NULL PRIMARY KEY,
+    reporter_member_id      UUID
+        CONSTRAINT fk_report_on_reporter REFERENCES public.member (member_id),
+    reported_user_member_id UUID
+        CONSTRAINT fk_report_on_reported_user REFERENCES public.member (member_id),
+    reason           VARCHAR(255)   NOT NULL
+        CONSTRAINT report_reason_check
+            CHECK ((reason)::TEXT = ANY ((ARRAY ['INAPPROPRIATE_LANGUAGE'::CHARACTER VARYING, 'FAKE_LISTING'::CHARACTER VARYING, 'SPAM'::CHARACTER VARYING])::TEXT[])),
+    description      TEXT,
+    status           VARCHAR(255)   NOT NULL DEFAULT 'PENDING'
+        CONSTRAINT report_status_check
+            CHECK ((status)::TEXT = ANY ((ARRAY ['PENDING'::CHARACTER VARYING, 'RESOLVED'::CHARACTER VARYING, 'REJECTED'::CHARACTER VARYING])::TEXT[])),
+    created_date     TIMESTAMPTZ(0) NOT NULL,
+    updated_date     TIMESTAMPTZ(0) NOT NULL
+);
