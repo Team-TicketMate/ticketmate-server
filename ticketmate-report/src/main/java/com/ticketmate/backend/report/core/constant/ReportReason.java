df150ad3908cf5c83a3064d75fd3040350f1a1c5
Yooonjeong <wkddbs3935@naver.com>
2025-09-11 22:25:30 +0900

    feat(user-report): ReportReason 반환값 수정

--- ticketmate-report/src/main/java/com/ticketmate/backend/report/core/constant/ReportReason.java
+++ ticketmate-report/src/main/java/com/ticketmate/backend/report/core/constant/ReportReason.java
@@ -1,11 +1,13 @@
 package com.ticketmate.backend.report.core.constant;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.ticketmate.backend.common.application.exception.CustomException;
 import com.ticketmate.backend.common.application.exception.ErrorCode;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+@JsonFormat(shape = JsonFormat.Shape.OBJECT)
 @Getter
 @RequiredArgsConstructor
 public enum ReportReason {
