55de0a74a9c09dd26b3b05fce0146e76826bfc0e
Yooonjeong <wkddbs3935@naver.com>
2025-09-11 23:30:58 +0900

    fix(user-report): flyway 코드 변경

--- ticketmate-api/src/main/resources/db/migration/V20250911_211310__create_report_table.sql
+++ ticketmate-api/src/main/resources/db/migration/V20250911_211310__create_report_table.sql
@@ -1,16 +1,16 @@
 -- Report(신고) 테이블 생성
 CREATE TABLE public.report
 (
-    report_id        UUID           NOT NULL PRIMARY KEY,
+    report_id               UUID           NOT NULL PRIMARY KEY,
     reporter_member_id      UUID
-        CONSTRAINT fk_report_on_reporter REFERENCES public.member (member_id),
+        CONSTRAINT fk_report_on_reporter REFERENCES member,
     reported_user_member_id UUID
-        CONSTRAINT fk_report_on_reported_user REFERENCES public.member (member_id),
+        CONSTRAINT fk_report_on_reported_user REFERENCES member,
     reason           VARCHAR(255)   NOT NULL
         CONSTRAINT report_reason_check
             CHECK ((reason)::TEXT = ANY ((ARRAY ['INAPPROPRIATE_LANGUAGE'::CHARACTER VARYING, 'FAKE_LISTING'::CHARACTER VARYING, 'SPAM'::CHARACTER VARYING])::TEXT[])),
     description      TEXT,
-    status           VARCHAR(255)   NOT NULL DEFAULT 'PENDING'
+    status           VARCHAR(255)   NOT NULL
         CONSTRAINT report_status_check
             CHECK ((status)::TEXT = ANY ((ARRAY ['PENDING'::CHARACTER VARYING, 'RESOLVED'::CHARACTER VARYING, 'REJECTED'::CHARACTER VARYING])::TEXT[])),
     created_date     TIMESTAMPTZ(0) NOT NULL,
