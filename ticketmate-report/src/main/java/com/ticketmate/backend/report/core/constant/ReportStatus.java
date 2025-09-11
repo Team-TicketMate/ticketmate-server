4a484249568133fcb894fc404beca75fa874afd2
Yooonjeong <wkddbs3935@naver.com>
2025-09-11 20:39:25 +0900

    refactor(user-report): ReportStatus에서 DELETED 제거

--- ticketmate-report/src/main/java/com/ticketmate/backend/report/core/constant/ReportStatus.java
+++ ticketmate-report/src/main/java/com/ticketmate/backend/report/core/constant/ReportStatus.java
@@ -3,7 +3,6 @@ package com.ticketmate.backend.report.core.constant;
 public enum ReportStatus {
   PENDING,  // 처리 대기
   RESOLVED, // 처리 완료
-  REJECTED,  // 기각
-  DELETED // 삭제됨
+  REJECTED  // 기각
   // TODO: 처리 상태 재정의
 }
