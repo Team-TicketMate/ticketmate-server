b4ba4b4b68fa6daaaa3f4c0b3cdf8dd058fe9690
Yooonjeong <wkddbs3935@naver.com>
2025-09-14 18:03:59 +0900

    refactor(user-report): flyway 내용 수정

--- ticketmate-api/src/main/resources/db/migration/V20250911_211310__create_report_table.sql
+++ ticketmate-api/src/main/resources/db/migration/V20250911_211310__create_report_table.sql
@@ -2,17 +2,17 @@
 CREATE TABLE public.report
 (
     report_id               UUID           NOT NULL PRIMARY KEY,
-    reporter_member_id      UUID
+    reporter_member_id      UUID           NOT NULL
         CONSTRAINT FK4u358afc7n9mo843563w364lp REFERENCES member,
-    reported_user_member_id UUID
+    reported_user_member_id UUID           NOT NULL
         CONSTRAINT FKcdhkgbi9qbj1gnpqg2o1f3ido REFERENCES member,
-    reason           VARCHAR(255)   NOT NULL
+    report_reason           VARCHAR(255)   NOT NULL
         CONSTRAINT report_reason_check
-            CHECK ((reason)::TEXT = ANY ((ARRAY ['INAPPROPRIATE_LANGUAGE'::CHARACTER VARYING, 'FAKE_LISTING'::CHARACTER VARYING, 'SPAM'::CHARACTER VARYING])::TEXT[])),
-    description      TEXT,
-    status           VARCHAR(255)   NOT NULL
+            CHECK ((report_reason)::TEXT = ANY ((ARRAY ['INAPPROPRIATE_LANGUAGE'::CHARACTER VARYING, 'FAKE_LISTING'::CHARACTER VARYING, 'SPAM'::CHARACTER VARYING])::TEXT[])),
+    description             VARCHAR(200),
+    report_status           VARCHAR(255)   NOT NULL
         CONSTRAINT report_status_check
-            CHECK ((status)::TEXT = ANY ((ARRAY ['PENDING'::CHARACTER VARYING, 'RESOLVED'::CHARACTER VARYING, 'REJECTED'::CHARACTER VARYING])::TEXT[])),
+            CHECK ((report_status)::TEXT = ANY ((ARRAY ['PENDING'::CHARACTER VARYING, 'IN_PROGRESS'::CHARACTER VARYING, 'RESOLVED'::CHARACTER VARYING, 'REJECTED'::CHARACTER VARYING])::TEXT[])),
     created_date     TIMESTAMPTZ(0) NOT NULL,
     updated_date     TIMESTAMPTZ(0) NOT NULL
 );
