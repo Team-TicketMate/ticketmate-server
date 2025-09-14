d30141509d2a615014e4e0e30d5b11e0ce0ca106
Yooonjeong <wkddbs3935@naver.com>
2025-09-14 17:59:19 +0900

    refactor(user-report): errorCode명 수정 및 추가

--- ticketmate-common/src/main/java/com/ticketmate/backend/common/application/exception/ErrorCode.java
+++ ticketmate-common/src/main/java/com/ticketmate/backend/common/application/exception/ErrorCode.java
@@ -254,7 +254,9 @@ public enum ErrorCode {
 
   INVALID_REPORT_REASON(HttpStatus.BAD_REQUEST, "유효하지 않은 신고 사유입니다."),
 
-  CANNOT_REPORT_SELF(HttpStatus.BAD_REQUEST, "자기 자신을 신고할 수 없습니다.")
+  SELF_REPORT_NOT_ALLOWED(HttpStatus.BAD_REQUEST, "자기 자신을 신고할 수 없습니다."),
+
+  REPORT_STATUS_TRANSITION_ERROR(HttpStatus.BAD_REQUEST, "신고 상태를 변경할 수 없습니다.")
 
   ;
 
