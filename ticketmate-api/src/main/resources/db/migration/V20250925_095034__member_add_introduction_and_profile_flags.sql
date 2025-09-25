fa04ae3f2304eb02413186b29acf8c750dd7ffd7
Chuseok22 <bjh59629@naver.com>
2025-09-25 10:05:31 +0900

    소셜_로그인_후_기본_프로필_설정_로직_추가 : feat : member 테이블 추가 필드에 따른 flyway migration sql 작성 https://github.com/Team-TicketMate/ticketmate-server/issues/538

---
+++ ticketmate-api/src/main/resources/db/migration/V20250925_095034__member_add_introduction_and_profile_flags.sql
@@ -0,0 +1,43 @@
+-- 1. 한줄소개 column 추가
+-- 2. 본인인증 여부 column 추가
+-- 3. 기본 프로필 설정 여부 column 추가
+
+-- 기존 데이터 백필 규칙 적용
+
+-- 컬럼 추가
+ALTER TABLE member
+  ADD COLUMN IF NOT EXISTS introduction varchar(20);
+
+ALTER TABLE member
+  ADD COLUMN IF NOT EXISTS is_phone_number_verified BOOLEAN;
+
+ALTER TABLE member
+  ADD COLUMN IF NOT EXISTS is_initial_profile_set BOOLEAN;
+
+-- 기존 데이터 백필
+UPDATE member
+SET introduction = NULL
+WHERE introduction IS NOT NULL
+  AND BTRIM(introduction) = '';
+
+UPDATE member
+SET is_phone_number_verified = FALSE
+WHERE is_phone_number_verified IS NULL;
+
+UPDATE member
+SET is_initial_profile_set = FALSE
+WHERE is_initial_profile_set ISNULL;
+
+-- 제약 및 기본값 설정
+-- 신규 레코드 기본값 FALSE로 설정되도록 DEFAULT 추가
+ALTER TABLE member
+  ALTER COLUMN is_phone_number_verified SET DEFAULT FALSE;
+
+ALTER TABLE member
+  ALTER COLUMN is_phone_number_verified SET NOT NULL;
+
+ALTER TABLE member
+  ALTER COLUMN is_initial_profile_set SET DEFAULT FALSE;
+
+ALTER TABLE member
+  ALTER COLUMN is_initial_profile_set SET NOT NULL;
