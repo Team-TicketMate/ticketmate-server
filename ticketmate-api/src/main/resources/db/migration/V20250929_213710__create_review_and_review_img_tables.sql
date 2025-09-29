0b8ddf00c960b692b4158ce7947f59a183255ab6
Yooonjeong <wkddbs3935@naver.com>
2025-09-29 22:01:48 +0900

    리뷰_CRUD_기능_개발 : feat : flyway 코드 수정 https://github.com/Team-TicketMate/ticketmate-server/issues/533

--- ticketmate-api/src/main/resources/db/migration/V20250929_213710__create_review_and_review_img_tables.sql
+++ ticketmate-api/src/main/resources/db/migration/V20250929_213710__create_review_and_review_img_tables.sql
@@ -1,13 +1,13 @@
 CREATE TABLE public.review
 (
     review_id           UUID           NOT NULL PRIMARY KEY,
-    application_form_id UUID           NOT NULL UNIQUE
+    application_form_application_form_id UUID           NOT NULL UNIQUE
         CONSTRAINT fk_review_on_application_form REFERENCES application_form,
-    client_id           UUID           NOT NULL
+    client_member_id           UUID           NOT NULL
         CONSTRAINT fk_review_on_client_member REFERENCES member,
-    agent_id            UUID           NOT NULL
+    agent_member_id            UUID           NOT NULL
         CONSTRAINT fk_review_on_agent_member REFERENCES member,
-    rating              REAL           NOT NULL,
+    rating              FLOAT           NOT NULL,
     comment             VARCHAR(300)   NOT NULL,
     created_date        TIMESTAMPTZ(0) NOT NULL,
     updated_date        TIMESTAMPTZ(0) NOT NULL
@@ -16,7 +16,7 @@ CREATE TABLE public.review
 CREATE TABLE public.review_img
 (
     review_img_id UUID           NOT NULL PRIMARY KEY,
-    review_id     UUID           NOT NULL
+    review_review_id     UUID           NOT NULL
         CONSTRAINT fk_review_img_on_review REFERENCES review,
     file_url      VARCHAR(255)   NOT NULL,
     stored_path   VARCHAR(255)   NOT NULL UNIQUE,
