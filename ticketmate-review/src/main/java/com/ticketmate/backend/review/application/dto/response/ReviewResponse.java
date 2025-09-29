0a5ad6cddfadfa3ca7ccc814acdb8fcd2971b67a
Yooonjeong <wkddbs3935@naver.com>
2025-09-29 21:34:15 +0900

    리뷰_CRUD_기능_개발 : feat : Review 및 ReviewImg Response DTO 정의 https://github.com/Team-TicketMate/ticketmate-server/issues/533

---
+++ ticketmate-review/src/main/java/com/ticketmate/backend/review/application/dto/response/ReviewResponse.java
@@ -0,0 +1,24 @@
+package com.ticketmate.backend.review.application.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+@Getter
+@AllArgsConstructor
+public class ReviewResponse {
+  private UUID reviewId;
+
+  private String concertTitle;
+
+  private float rating;
+
+  private String comment;
+
+  private List<ReviewImgResponse> reviewImgList;
+
+  private LocalDateTime createdDate;
+}
