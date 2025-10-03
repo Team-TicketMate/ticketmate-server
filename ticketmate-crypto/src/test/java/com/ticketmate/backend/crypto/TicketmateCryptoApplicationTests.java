cfadb2499b212584266693b7ed803ec75ee379b0
정우혁 <93671010+mr6208@users.noreply.github.com>
2025-10-03 20:57:46 +0900

    feat(bank-account): 암호화 모듈 설계

---
+++ ticketmate-crypto/src/test/java/com/ticketmate/backend/crypto/TicketmateCryptoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ticketmate.backend.crypto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketmateCryptoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
