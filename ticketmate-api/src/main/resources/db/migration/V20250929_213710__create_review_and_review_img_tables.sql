cc81733f18820a0e26573f87fbdfe7e65279b8c3
Yooonjeong <wkddbs3935@naver.com>
2025-10-06 00:00:52 +0900

    리뷰_CRUD_기능_개발 : fix : flyway 제약조건명 제거 https://github.com/Team-TicketMate/ticketmate-server/issues/533

--- ticketmate-api/src/main/resources/db/migration/V20250929_213710__create_review_and_review_img_tables.sql
+++ ticketmate-api/src/main/resources/db/migration/V20250929_213710__create_review_and_review_img_tables.sql
@@ -1,12 +1,9 @@
 CREATE TABLE public.review
 (
     review_id                               UUID           NOT NULL PRIMARY KEY,
-    application_form_application_form_id    UUID           NOT NULL UNIQUE
-        CONSTRAINT fk_review_on_application_form REFERENCES application_form,
-    client_member_id                        UUID           NOT NULL
-        CONSTRAINT fk_review_on_client_member REFERENCES member,
-    agent_member_id                         UUID           NOT NULL
-        CONSTRAINT fk_review_on_agent_member REFERENCES member,
+    application_form_application_form_id    UUID           NOT NULL UNIQUE REFERENCES application_form,
+    client_member_id                        UUID           NOT NULL REFERENCES member,
+    agent_member_id                         UUID           NOT NULL REFERENCES member,
     rating                                  REAL           NOT NULL,
     comment                                 VARCHAR(300)   NOT NULL,
     agent_comment                           VARCHAR(300)   NULL,
@@ -18,11 +15,10 @@ CREATE TABLE public.review
 CREATE TABLE public.review_img
 (
     review_img_id                           UUID           NOT NULL PRIMARY KEY,
-    review_review_id                        UUID           NOT NULL
-        CONSTRAINT fk_review_img_on_review REFERENCES review,
+    review_review_id                        UUID           NOT NULL REFERENCES review,
     original_filename                       VARCHAR(255)   NOT NULL,
     stored_path                             VARCHAR(255)   NOT NULL UNIQUE,
-    file_extension                          VARCHAR(255)     NOT NULL
+    file_extension                          VARCHAR(255)   NOT NULL
         constraint review_img_file_extension_check
             check ((file_extension)::text = ANY ((ARRAY ['JPG'::character varying, 'JPEG'::character varying, 'PNG'::character varying, 'GIF'::character varying, 'BMP'::character varying, 'WEBP'::character varying])::text[])),
     size_bytes                              BIGINT         NOT NULL,
