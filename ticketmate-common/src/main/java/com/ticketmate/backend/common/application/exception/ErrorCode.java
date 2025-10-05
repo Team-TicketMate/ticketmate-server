9f4b4e0dd93322f69a1c018429121d5bd3285905
Yooonjeong <wkddbs3935@naver.com>
2025-10-05 22:55:53 +0900

    리뷰_CRUD_기능_개발 : feat : 리뷰 댓글 작성 권한 ErrorCode 추가 https://github.com/Team-TicketMate/ticketmate-server/issues/533

--- ticketmate-common/src/main/java/com/ticketmate/backend/common/application/exception/ErrorCode.java
+++ ticketmate-common/src/main/java/com/ticketmate/backend/common/application/exception/ErrorCode.java
@@ -262,6 +262,8 @@ public enum ErrorCode {
 
   NO_AUTH_TO_REVIEW(HttpStatus.FORBIDDEN, "해당 신청서에 대한 리뷰를 작성할 권한이 없습니다."),
 
+  NO_AUTH_TO_REVIEW_COMMENT(HttpStatus.FORBIDDEN, "해당 리뷰에 댓글을 작성할 권한이 없습니다."),
+
   CANNOT_REVIEW_NOT_SUCCEEDED_FORM(HttpStatus.BAD_REQUEST, "성공한 신청서에 대해서만 리뷰를 작성할 수 있습니다."),
 
   REVIEW_ALREADY_EXISTS(HttpStatus.CONFLICT, "이미 해당 신청서에 대한 리뷰가 존재합니다."),
