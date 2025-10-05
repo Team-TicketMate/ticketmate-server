d9c62d6cc0670e9a50977ebae3e4d238479dcd56
Yooonjeong <wkddbs3935@naver.com>
2025-10-05 23:50:31 +0900

    리뷰_CRUD_기능_개발 : comment : 주석 제거 https://github.com/Team-TicketMate/ticketmate-server/issues/533

--- ticketmate-review/src/main/java/com/ticketmate/backend/review/core/constant/ReviewSortField.java
+++ ticketmate-review/src/main/java/com/ticketmate/backend/review/core/constant/ReviewSortField.java
@@ -12,12 +12,7 @@ public enum ReviewSortField implements SortField {
   RATING("rating");
 
   private final String property;
-
-  /**
-   * Jackson이 JSON -> Java 객체로 역직렬화 (deserialization)할 때 자동 호출
-   * 컨트롤러에서 들어온 {"sortField": "TICKET_OPEN_DATE"} 같은 문자열을 변환
-   * 만약 {"sortField": "ticketOpenDate"}와 같이 카멜케이스로 들어와도 f.property와 비교하여 자동 매칭
-   */
+  
   public static ReviewSortField from(String value) {
     return CommonUtil.stringToSortField(ReviewSortField.class, value);
   }
