1b696176dd241d437abdc8bd20039a51c771dd7a
Yooonjeong <wkddbs3935@naver.com>
2025-10-05 20:15:35 +0900

    리뷰_CRUD_기능_개발 : fix : review flyway 필드 수정 https://github.com/Team-TicketMate/ticketmate-server/issues/533

--- ticketmate-api/src/main/resources/db/migration/V20250929_213710__create_review_and_review_img_tables.sql
+++ ticketmate-api/src/main/resources/db/migration/V20250929_213710__create_review_and_review_img_tables.sql
@@ -1,25 +1,29 @@
 CREATE TABLE public.review
 (
-    review_id           UUID           NOT NULL PRIMARY KEY,
-    application_form_application_form_id UUID           NOT NULL UNIQUE
+    review_id                               UUID           NOT NULL PRIMARY KEY,
+    application_form_application_form_id    UUID           NOT NULL UNIQUE
         CONSTRAINT fk_review_on_application_form REFERENCES application_form,
-    client_member_id           UUID           NOT NULL
+    client_member_id                        UUID           NOT NULL
         CONSTRAINT fk_review_on_client_member REFERENCES member,
-    agent_member_id            UUID           NOT NULL
+    agent_member_id                         UUID           NOT NULL
         CONSTRAINT fk_review_on_agent_member REFERENCES member,
-    rating              FLOAT           NOT NULL,
-    comment             VARCHAR(300)   NOT NULL,
-    created_date        TIMESTAMPTZ(0) NOT NULL,
-    updated_date        TIMESTAMPTZ(0) NOT NULL
+    rating                                  REAL           NOT NULL,
+    comment                                 VARCHAR(300)   NOT NULL,
+    created_date                            TIMESTAMPTZ(0) NOT NULL,
+    updated_date                            TIMESTAMPTZ(0) NOT NULL
 );
 
 CREATE TABLE public.review_img
 (
-    review_img_id UUID           NOT NULL PRIMARY KEY,
-    review_review_id     UUID           NOT NULL
+    review_img_id                           UUID           NOT NULL PRIMARY KEY,
+    review_review_id                        UUID           NOT NULL
         CONSTRAINT fk_review_img_on_review REFERENCES review,
-    file_url      VARCHAR(255)   NOT NULL,
-    stored_path   VARCHAR(255)   NOT NULL UNIQUE,
-    created_date  TIMESTAMPTZ(0) NOT NULL,
-    updated_date  TIMESTAMPTZ(0) NOT NULL
+    original_filename                       VARCHAR(255)   NOT NULL,
+    stored_path                             VARCHAR(255)   NOT NULL UNIQUE,
+    file_extension                          VARCHAR(255)     NOT NULL
+        constraint review_img_file_extension_check
+            check ((file_extension)::text = ANY ((ARRAY ['JPG'::character varying, 'JPEG'::character varying, 'PNG'::character varying, 'GIF'::character varying, 'BMP'::character varying, 'WEBP'::character varying])::text[])),
+    size_bytes                              BIGINT         NOT NULL,
+    created_date                            TIMESTAMPTZ(0) NOT NULL,
+    updated_date                            TIMESTAMPTZ(0) NOT NULL
 );
