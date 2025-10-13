ceee3db1324992371392db360b5eb2e0e62b6375
Chuseok22 <bjh59629@naver.com>
2025-10-13 09:46:21 +0900

    예매_기간이_지난_공연_정보_조회_시_null_반환하던_문제_해결 : feat : ApplicationFormDetail -> ApplicationFormDetailResponse 변환 매퍼 MapStruct 미사용 https://github.com/Team-TicketMate/ticketmate-server/issues/543

--- ticketmate-applicationform/src/main/java/com/ticketmate/backend/applicationform/application/mapper/ApplicationFormMapStruct.java
+++ ticketmate-applicationform/src/main/java/com/ticketmate/backend/applicationform/application/mapper/ApplicationFormMapStruct.java
@@ -1,10 +1,8 @@
 package com.ticketmate.backend.applicationform.application.mapper;
 
 import com.ticketmate.backend.applicationform.application.dto.request.HopeAreaRequest;
-import com.ticketmate.backend.applicationform.application.dto.response.ApplicationFormDetailResponse;
 import com.ticketmate.backend.applicationform.application.dto.response.HopeAreaResponse;
 import com.ticketmate.backend.applicationform.application.dto.response.RejectionReasonResponse;
-import com.ticketmate.backend.applicationform.infrastructure.entity.ApplicationFormDetail;
 import com.ticketmate.backend.applicationform.infrastructure.entity.HopeArea;
 import com.ticketmate.backend.applicationform.infrastructure.entity.RejectionReason;
 import java.util.List;
@@ -21,9 +19,6 @@ public interface ApplicationFormMapStruct {
 
   // List<HopeArea> -> List<HopeAreaResponse> (엔티티 리스트 -> DTO 리스트)
   List<HopeAreaResponse> toHopeAreaResponseList(List<HopeArea> hopeAreaList);
-
-  // List<ApplicationFormDetail> -> List<ApplicationFormDetailResponse> (엔티티 리스트 -> DTO 리스트)
-  List<ApplicationFormDetailResponse> toApplicationFormDetailResponseList(List<ApplicationFormDetail> applicationFormDetailList);
 
   // RejectionReason -> RejectionReasonResponse (엔티티 -> DTO)
   RejectionReasonResponse toRejectionReasonResponse(RejectionReason rejectionReason);
