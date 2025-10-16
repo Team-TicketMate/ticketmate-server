d598496e735a885f2d6077dc4e98500991151434
정우혁 <93671010+mr6208@users.noreply.github.com>
2025-10-16 18:26:08 +0900

    feat(bank-account): AgentBankAccountConstant 전용 상수값 추가

--- ticketmate-member/src/main/java/com/ticketmate/backend/member/infrastructure/constant/AgentBankAccountConstant.java
+++ ticketmate-member/src/main/java/com/ticketmate/backend/member/infrastructure/constant/AgentBankAccountConstant.java
@@ -5,7 +5,9 @@ import lombok.experimental.UtilityClass;
 
 @UtilityClass
 public class AgentBankAccountConstant {
-  public static final int MAX_ACCOUNT_NUMBER = 5;
+  public static final int MAX_ACCOUNT_COUNT = 5;
+
+  public static final int MAX_ACCOUNT_HOLDER_LENGTH = 20;
 
   public static final Pattern ACCOUNT_NUMBER_PATTERN = Pattern.compile("^[0-9]{11,16}$");
 }
