9614f7ecd7efd32af9f84af26a07243920719323
Chuseok22 <bjh59629@naver.com>
2025-10-23 11:20:45 +0900

    서버_기동시_flyway_스크립트_미적용_문제 : feat : AesGcmConfig.java 내부 aesGcmInit Bean 초기화 순서 지정 제거 https://github.com/Team-TicketMate/ticketmate-server/issues/561

--- ticketmate-crypto/src/main/java/com/ticketmate/backend/crypto/infrastructure/config/AesGcmConfig.java
+++ ticketmate-crypto/src/main/java/com/ticketmate/backend/crypto/infrastructure/config/AesGcmConfig.java
@@ -4,6 +4,8 @@ import com.ticketmate.backend.common.application.exception.CustomException;
 import com.ticketmate.backend.common.application.exception.ErrorCode;
 import com.ticketmate.backend.crypto.infrastructure.properties.AesGcmProperties;
 import com.ticketmate.backend.crypto.infrastructure.provider.AesGcmProvider;
+import java.util.Arrays;
+import java.util.Base64;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -14,27 +16,26 @@ import org.springframework.context.annotation.Configuration;
 @RequiredArgsConstructor
 @EnableConfigurationProperties(AesGcmProperties.class)
 public class AesGcmConfig {
+
   private final AesGcmProperties properties;
 
-  /** 키 초기화 (정적) */
+  /**
+   * 키 초기화 (정적)
+   */
   @Bean(name = "aesGcmInit")
   public InitializingBean aesGcmInit() {
     return () -> {
       // (선택) 길이 검증
-      byte[] raw = java.util.Base64.getDecoder().decode(properties.secretKey());
-      if (raw.length != 32) throw new CustomException(ErrorCode.AES_KEY_LENGTH_INVALID);
+      byte[] raw = Base64.getDecoder().decode(properties.secretKey());
+      if (raw.length != 32) {
+        throw new CustomException(ErrorCode.AES_KEY_LENGTH_INVALID);
+      }
 
-      // 정적 프로바이더 초기화
-      AesGcmProvider.initFromSecretKey(new javax.crypto.spec.SecretKeySpec(raw, "AES"));
-    };
-  }
-
-  /** JPA(SessionFactory) 생성 전에 키 초기화 보장 */
-  @Bean
-  public static org.springframework.beans.factory.config.BeanFactoryPostProcessor forceInitOrder() {
-    return bf -> {
-      if (bf.containsBeanDefinition("entityManagerFactory")) {
-        bf.getBeanDefinition("entityManagerFactory").setDependsOn("aesGcmInit");
+      try {
+        // 정적 프로바이더 초기화
+        AesGcmProvider.initFromSecretKey(new javax.crypto.spec.SecretKeySpec(raw, "AES"));
+      } finally {
+        Arrays.fill(raw, (byte) 0);
       }
     };
   }
