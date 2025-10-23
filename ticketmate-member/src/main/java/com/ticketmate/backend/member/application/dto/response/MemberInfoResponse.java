fe0ed31b1d5d28c3454b90f7292f84f04efd9104
Chuseok22 <bjh59629@naver.com>
2025-10-23 15:04:52 +0900

    GET_api_member_회원정보_조회_시_profileUrl_값_매핑_버그 : feat : MemberMapper MapStruct -> 수동 매퍼 사용으로의 변경 https://github.com/Team-TicketMate/ticketmate-server/issues/560

--- ticketmate-member/src/main/java/com/ticketmate/backend/member/application/dto/response/MemberInfoResponse.java
+++ ticketmate-member/src/main/java/com/ticketmate/backend/member/application/dto/response/MemberInfoResponse.java
@@ -2,55 +2,21 @@ package com.ticketmate.backend.member.application.dto.response;
 
 import com.ticketmate.backend.member.core.constant.MemberType;
 import java.util.UUID;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-@Getter
-@Setter
-@Builder
-@AllArgsConstructor
-@NoArgsConstructor
-public class MemberInfoResponse {
+public record MemberInfoResponse(
+    UUID memberId, // 회원 PK
+    String username, // 이메일
+    String nickname, // 닉네임
+    String name, // 이름
+    String birthDay, // 생일
+    String birthYear, // 출생연도
+    String phone, // 전화번호
+    String profileUrl, // 프로필 이미지
+    String gender, // 성별
+    String introduction, // 한줄 소개
+    MemberType memberType, // 회원 종류 (대리인, 구매자)
+    long followingCount, // 팔로잉 수 (내가 팔로잉하는 수)
+    long followerCount // 팔로워 수 (나를 팔로우하는 수)
+) {
 
-  // 회원 PK
-  private UUID memberId;
-
-  // 이메일
-  private String username;
-
-  // 닉네임
-  private String nickname;
-
-  // 이름
-  private String name;
-
-  // 생일
-  private String birthDay;
-
-  // 출생연도
-  private String birthYear;
-
-  // 전화번호
-  private String phone;
-
-  // 프로필 이미지
-  private String profileUrl;
-
-  // 성별
-  private String gender;
-
-  // 한줄 소개
-  private String introduction;
-
-  // 회원 종류 (대리인, 구매자)
-  private MemberType memberType;
-
-  // 팔로잉 수 (내가 팔로잉하는 수)
-  private long followingCount;
-
-  // 팔로워 수 (나를 팔로우하는 수)
-  private long followerCount;
 }
--- ticketmate-member/src/main/java/com/ticketmate/backend/member/application/mapper/MemberMapper.java
+++ ticketmate-member/src/main/java/com/ticketmate/backend/member/application/mapper/MemberMapper.java
@@ -2,14 +2,9 @@ package com.ticketmate.backend.member.application.mapper;
 
 import com.ticketmate.backend.member.application.dto.response.MemberInfoResponse;
 import com.ticketmate.backend.member.infrastructure.entity.Member;
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 
-@Mapper(componentModel = "spring")
 public interface MemberMapper {
 
   // Member -> MemberInfoResponse (DTO)
-  @Mapping(target = "introduction", source = "introduction", defaultValue = "")
   MemberInfoResponse toMemberInfoResponse(Member member);
-
 }
