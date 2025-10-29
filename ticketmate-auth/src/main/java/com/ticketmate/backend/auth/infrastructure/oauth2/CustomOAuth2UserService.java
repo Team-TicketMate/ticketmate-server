766c3b2743e3607883f849d0193f64e11ae43cf1
Chuseok22 <bjh59629@naver.com>
2025-10-29 23:38:01 +0900

    회원_탈퇴_기능_추가 : feat : 로그인 및 회원가입 과정에서 차단 여부 검증 로직 추가 https://github.com/Team-TicketMate/ticketmate-server/issues/581

--- ticketmate-auth/src/main/java/com/ticketmate/backend/auth/infrastructure/oauth2/CustomOAuth2UserService.java
+++ ticketmate-auth/src/main/java/com/ticketmate/backend/auth/infrastructure/oauth2/CustomOAuth2UserService.java
@@ -1,11 +1,15 @@
 package com.ticketmate.backend.auth.infrastructure.oauth2;
 
+import com.ticketmate.backend.auth.application.validator.AuthValidator;
 import com.ticketmate.backend.auth.core.response.OAuth2Response;
 import com.ticketmate.backend.auth.infrastructure.oauth2.response.KakaoResponse;
 import com.ticketmate.backend.auth.infrastructure.oauth2.response.NaverResponse;
 import com.ticketmate.backend.common.application.exception.CustomException;
 import com.ticketmate.backend.common.application.exception.ErrorCode;
+import com.ticketmate.backend.common.core.util.CommonUtil;
 import com.ticketmate.backend.common.infrastructure.util.TimeUtil;
+import com.ticketmate.backend.member.application.service.PhoneBlockService;
+import com.ticketmate.backend.member.core.constant.AccountStatus;
 import com.ticketmate.backend.member.core.constant.MemberType;
 import com.ticketmate.backend.member.core.constant.Role;
 import com.ticketmate.backend.member.core.constant.SocialPlatform;
@@ -20,6 +24,7 @@ import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserServ
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -27,9 +32,12 @@ import org.springframework.stereotype.Service;
 public class CustomOAuth2UserService extends DefaultOAuth2UserService {
 
   private final MemberRepository memberRepository;
+  private final PhoneBlockService phoneBlockService;
+  private final AuthValidator authValidator;
   private final ZoneId zoneId;
 
   @Override
+  @Transactional
   public OAuth2User loadUser(OAuth2UserRequest request) {
     OAuth2User oAuth2User = super.loadUser(request);
     log.debug("요청된 OAuth2User: {}", oAuth2User.getAttributes());
@@ -48,8 +56,14 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
       }
     }
 
+    // 소셜 로그인 시 전화번호 차단 검증
+    if (!CommonUtil.nvl(oAuth2Response.getPhone(), "").isEmpty()) {
+      phoneBlockService.ensurePhoneNotBlocked(oAuth2Response.getPhone());
+    }
+
     // Member 확인
-    Optional<Member> optionalMember = memberRepository.findByUsername(oAuth2Response.getEmail());
+    Optional<Member> optionalMember = memberRepository
+        .findByUsernameAndDeletedFalse(oAuth2Response.getEmail());
     Member member;
     if (optionalMember.isEmpty()) { // 첫 로그인한 회원인 경우
       member = Member.builder()
@@ -66,16 +80,22 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
           .introduction(null)
           .role(Role.ROLE_USER)
           .memberType(MemberType.CLIENT)
+          .accountStatus(AccountStatus.ACTIVE)
           .isFirstLogin(true)
           .lastLoginTime(TimeUtil.now())
           .isPhoneNumberVerified(false)
           .isInitialProfileSet(false)
           .followingCount(0L)
           .followerCount(0L)
+          .deleted(false)
+          .deletedDate(null)
           .build();
     } else { // 첫 로그인이 아닌경우
       member = optionalMember.get();
-      if (!member.getSocialPlatform().equals(socialPlatform)) { // 기존 가입된 소셜 플랫폼과 다른경우
+      // 로그인 가능 여부 검증
+      authValidator.assertLoginAllowed(member);
+
+      if (member.getSocialPlatform() != socialPlatform) { // 기존 가입된 소셜 플랫폼과 다른경우
         log.error("기존에 가입한 소셜 로그인 플랫폼이 아닙니다. 현재 요청한 소셜 플랫폼: {}, 기존 회원가입된 소셜 플랫폼: {}",
             socialPlatform, member.getSocialPlatform());
         throw new CustomException(ErrorCode.INVALID_SOCIAL_PLATFORM);
@@ -89,9 +109,10 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
   }
 
   // 회원 이메일을 통한 CustomOAuth2User 반환
+  @Transactional(readOnly = true)
   public CustomOAuth2User loadUserByUsername(String username) {
 
-    Member member = memberRepository.findByUsername(username)
+    Member member = memberRepository.findByUsernameAndDeletedFalse(username)
         .orElseThrow(() -> {
           log.error("회원을 찾을 수 없습니다. 요청 username: {}", username);
           return new CustomException(ErrorCode.MEMBER_NOT_FOUND);
