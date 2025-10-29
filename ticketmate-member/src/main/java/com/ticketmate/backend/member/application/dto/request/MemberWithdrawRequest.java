9351852fd79cb10a193edb17abefcb01a9c8fba9
Chuseok22 <bjh59629@naver.com>
2025-10-29 23:31:41 +0900

    회원_탈퇴_기능_추가 : feat : 회원 탈퇴 로직 추가 (MemberWithdrawalHistoryService.java) https://github.com/Team-TicketMate/ticketmate-server/issues/581

---
+++ ticketmate-member/src/main/java/com/ticketmate/backend/member/application/dto/request/MemberWithdrawRequest.java
@@ -0,0 +1,24 @@
+package com.ticketmate.backend.member.application.dto.request;
+
+import com.ticketmate.backend.member.core.constant.WithdrawalReasonType;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class MemberWithdrawRequest {
+
+  @NotNull(message = "회원탈퇴 사유를 입력하세요")
+  private WithdrawalReasonType withdrawalReasonType;
+
+  @Size(max = 20, message = "기타 사유는 최대 20자까지 작성 가능합니다")
+  private String otherReason;
+}
