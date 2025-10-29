cd9330deb6daf9b42e0295b5ef2f104054d1838c
Chuseok22 <bjh59629@naver.com>
2025-10-29 23:35:04 +0900

    회원_탈퇴_기능_추가 : feat : 로그인 검증 로직 (AuthValidator.java) 추가 https://github.com/Team-TicketMate/ticketmate-server/issues/581

---
+++ ticketmate-auth/src/main/java/com/ticketmate/backend/auth/application/validator/AuthValidator.java
@@ -0,0 +1,47 @@
+package com.ticketmate.backend.auth.application.validator;
+
+import static com.ticketmate.backend.member.core.constant.AccountStatus.ACTIVE;
+
+import com.ticketmate.backend.common.application.exception.CustomException;
+import com.ticketmate.backend.common.application.exception.ErrorCode;
+import com.ticketmate.backend.member.core.constant.AccountStatus;
+import com.ticketmate.backend.member.infrastructure.entity.Member;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.convert.ReadingConverter;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@ReadingConverter
+public class AuthValidator {
+
+  /**
+   * 로그인 가능 여부 boolean
+   */
+  public boolean isLoginAllowed(Member member) {
+    return member.getAccountStatus() == ACTIVE;
+  }
+
+  /**
+   * 로그인 가능 여부 검증 exception
+   */
+  public void assertLoginAllowed(Member member) {
+    if (!isLoginAllowed(member)) {
+      log.warn("로그인 불가 상태의 계정입니다. accountStatus: {}, memberId: {}", member.getAccountStatus(), member.getMemberId());
+      ErrorCode errorCode = resolveLoginRestrictionErrorCode(member.getAccountStatus());
+      throw new CustomException(errorCode);
+    }
+  }
+
+  /**
+   * 비활성 계정 ErrorCode 반환
+   */
+  public ErrorCode resolveLoginRestrictionErrorCode(AccountStatus accountStatus) {
+    return switch (accountStatus) {
+      case ACTIVE -> ErrorCode.INVALID_REQUEST;
+      case WITHDRAWN -> ErrorCode.ACCOUNT_WITHDRAWN;
+      case TEMP_BAN -> ErrorCode.ACCOUNT_TEMP_BANNED;
+      case PERMANENT_BAN -> ErrorCode.ACCOUNT_PERMANENT_BANNED;
+    };
+  }
+}
