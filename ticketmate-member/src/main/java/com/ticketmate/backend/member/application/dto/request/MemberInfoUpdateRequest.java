ac2a85df722271543ba01502adbeb92b5738cdc5
Chuseok22 <bjh59629@naver.com>
2025-10-30 13:12:44 +0900

    회원_탈퇴_기능_추가 : feat : 회원정보 수정 DTO 닉네임 글자수 validation 추가 https://github.com/Team-TicketMate/ticketmate-server/issues/581

--- ticketmate-member/src/main/java/com/ticketmate/backend/member/application/dto/request/MemberInfoUpdateRequest.java
+++ ticketmate-member/src/main/java/com/ticketmate/backend/member/application/dto/request/MemberInfoUpdateRequest.java
@@ -1,5 +1,8 @@
 package com.ticketmate.backend.member.application.dto.request;
 
+import static com.ticketmate.backend.member.core.constant.MemberInfoConstants.NICKNAME_MAX_LENGTH;
+import static com.ticketmate.backend.member.core.constant.MemberInfoConstants.NICKNAME_MIN_LENGTH;
+
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -15,6 +18,7 @@ import org.springframework.web.multipart.MultipartFile;
 @AllArgsConstructor
 public class MemberInfoUpdateRequest {
 
+  @Size(min = NICKNAME_MIN_LENGTH, max = NICKNAME_MAX_LENGTH, message = "닉네임은 최소2자 최대12까지 입력 가능합니다.")
   private String nickname;
 
   private MultipartFile profileImg;
