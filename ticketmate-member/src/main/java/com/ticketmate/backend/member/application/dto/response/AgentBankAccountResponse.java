0ad2bbc4bf382517a75971e3de9833c21fb394bc
정우혁 <93671010+mr6208@users.noreply.github.com>
2025-11-07 17:31:09 +0900

    feat(hot-fix): 은행정보 반환 데이터 타입 수정

--- ticketmate-member/src/main/java/com/ticketmate/backend/member/application/dto/response/AgentBankAccountResponse.java
+++ ticketmate-member/src/main/java/com/ticketmate/backend/member/application/dto/response/AgentBankAccountResponse.java
@@ -1,5 +1,6 @@
 package com.ticketmate.backend.member.application.dto.response;
 
+import com.ticketmate.backend.member.core.constant.BankCode;
 import java.util.UUID;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -11,7 +12,7 @@ import lombok.NoArgsConstructor;
 public class AgentBankAccountResponse {
   private UUID agentBankAccountId;  // 계좌 PK
   private String agentAccountNumber;  // 계좌 번호
-  private String bankName;  // 은행 이름
+  private BankCode bankCode;  // 은행 정보
   private boolean primaryAccount;  // 대표계좌 여부
   private String accountHolder;  // 예금주
 }
