27ab2d7250afbfa4304e7298aa5c74ceacb23f2e
Yooonjeong <wkddbs3935@naver.com>
2025-11-08 14:52:47 +0900

    대리인_수락_on_off_포함한_공연_리스트_API_구현 : refactor : Response 및 View DTO에서 status 제거 https://github.com/Team-TicketMate/ticketmate-server/issues/566

--- ticketmate-concertagentavailability/src/main/java/com/ticketmate/backend/concertagentavailability/application/dto/view/AgentConcertSettingInfo.java
+++ ticketmate-concertagentavailability/src/main/java/com/ticketmate/backend/concertagentavailability/application/dto/view/AgentConcertSettingInfo.java
@@ -6,7 +6,6 @@ public record AgentConcertSettingInfo(
   UUID concertId, // 공연 PK
   String concertName, // 공연 제목
   String concertThumbnailStoredPath, // 공연 썸네일 URL
-  Integer status, // 모집 여부
   Integer matchedClientCount, // 매칭된 의뢰인 수
   Boolean accepting // on/off
 ) {
