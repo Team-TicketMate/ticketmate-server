3d18648f5ea74f18d0d13623b5cb31071fba8021
Yooonjeong <wkddbs3935@naver.com>
2025-11-10 22:05:24 +0900

    반환_타입_Slice_List_변경_및_중복_방지_유니크_제약_추가 : feat : flyway 유니크 조건 설정 전 중복 데이터 검증 로직 추가 https://github.com/Team-TicketMate/ticketmate-server/issues/611

--- ticketmate-api/src/main/resources/db/migration/V20251110_210730__add_unique_constraint_to_concert_agent_availability.sql
+++ ticketmate-api/src/main/resources/db/migration/V20251110_210730__add_unique_constraint_to_concert_agent_availability.sql
@@ -1,3 +1,25 @@
+-- 중복 데이터 존재 여부 검증
+DO
+$$
+    DECLARE
+    v_count BIGINT;
+    BEGIN
+        SELECT COUNT(*)
+        INTO v_count
+        FROM (
+                 SELECT 1
+                 FROM public.concert_agent_availability
+                 GROUP BY concert_concert_id, agent_member_id
+                 HAVING COUNT(*) > 1
+             ) AS duplicate_check;
+
+        IF v_count > 0 THEN
+                    RAISE EXCEPTION
+                        'Flyway 마이그레이션 에러: concert_agent_availability 테이블에 중복되는 (concert_concert_id, agent_member_id) 조합이 %개 존재합니다.', v_count;
+        END IF;
+    END;
+$$;
+
 -- concert, agent 유니크 인덱스 생성
 CREATE UNIQUE INDEX ux_concert_agent_availability_concert_agent
     ON public.concert_agent_availability (concert_concert_id, agent_member_id);
