ea536eee86b681894416c7566ee55ecadbdbcbf8
Chuseok22 <bjh59629@naver.com>
2025-11-11 16:17:23 +0900

    개발용_알림_테스트_api_추가 : feat : 테스트 알림 발송 로직 추가 https://github.com/Team-TicketMate/ticketmate-server/issues/624

---
+++ ticketmate-mock/src/main/java/com/ticketmate/backend/mock/application/dto/request/MockNotificationRequest.java
@@ -0,0 +1,19 @@
+package com.ticketmate.backend.mock.application.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class MockNotificationRequest {
+
+  private String title;
+
+  private String body;
+}
