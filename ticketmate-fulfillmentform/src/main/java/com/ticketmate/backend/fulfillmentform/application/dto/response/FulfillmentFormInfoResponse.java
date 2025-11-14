7db81449f1391e74b83ddc2ce9efd867678a0149
정우혁 <93671010+mr6208@users.noreply.github.com>
2025-11-14 17:00:46 +0900

    티켓팅_성공_및_실패_관련된_기능_개발 : feat : 성공양식관련 DTO 설계 https://github.com/Team-TicketMate/ticketmate-server/issues/534

---
+++ ticketmate-fulfillmentform/src/main/java/com/ticketmate/backend/fulfillmentform/application/dto/response/FulfillmentFormInfoResponse.java
@@ -0,0 +1,22 @@
+package com.ticketmate.backend.fulfillmentform.application.dto.response;
+
+import com.ticketmate.backend.member.application.dto.response.AgentBankAccountResponse;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class FulfillmentFormInfoResponse {
+
+  private UUID fulfillmentFormId;
+  private List<FulfillmentFormImgResponse> fulfillmentFormImgUrlList = new ArrayList<>();
+  private String particularMemo;
+  private AgentBankAccountResponse agentBankAccount;
+  private LocalDateTime createDate;
+}
