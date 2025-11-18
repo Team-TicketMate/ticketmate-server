7ad7e64243325c4413fd4e463578480a483aa0bf
정우혁 <93671010+mr6208@users.noreply.github.com>
2025-11-18 17:11:00 +0900

    티켓팅_성공_및_실패_관련된_기능_개발 : feat : DTO 에노테이션 통일 https://github.com/Team-TicketMate/ticketmate-server/issues/534

--- ticketmate-chat/src/main/java/com/ticketmate/backend/chat/application/dto/request/FulfillmentFormMessageRequest.java
+++ ticketmate-chat/src/main/java/com/ticketmate/backend/chat/application/dto/request/FulfillmentFormMessageRequest.java
@@ -2,25 +2,23 @@ package com.ticketmate.backend.chat.application.dto.request;
 
 import com.ticketmate.backend.chat.core.constant.ChatMessageType;
 import java.util.UUID;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public final class FulfillmentFormMessageRequest implements ChatMessageRequest {
 
-  private final UUID fulfillmentFormId;
-  private final ChatMessageType chatMessageType;
-  private final String preview;
-  private final String rejectMemo;
-
-
-  @Builder
-  public FulfillmentFormMessageRequest(UUID fulfillmentFormId, ChatMessageType chatMessageType, String preview, String rejectMemo) {
-    this.fulfillmentFormId = fulfillmentFormId;
-    this.chatMessageType = chatMessageType;
-    this.preview = preview;
-    this.rejectMemo = rejectMemo;
-  }
+  private UUID fulfillmentFormId;
+  private ChatMessageType chatMessageType;
+  private String preview;
+  private String rejectMemo;
 
   /**
    * 성공양식이 전송됐다는 메시지를 발송하기위한 플래그(티켓팅 성공했다는 뜻)
