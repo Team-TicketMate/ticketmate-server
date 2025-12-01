7f9728ade2423ffacec17303f075217ae3d87fa5
정우혁 <93671010+mr6208@users.noreply.github.com>
2025-12-01 21:56:18 +0900

    hot-fix : 재재수정 불가 버그 수정

--- ticketmate-fulfillmentform/src/main/java/com/ticketmate/backend/fulfillmentform/infrastructure/constant/FulfillmentFormConstants.java
+++ ticketmate-fulfillmentform/src/main/java/com/ticketmate/backend/fulfillmentform/infrastructure/constant/FulfillmentFormConstants.java
@@ -18,6 +18,7 @@ public class FulfillmentFormConstants {
 
   // 수정 가능한 신청서 상태
   public static final EnumSet<FulfillmentFormStatus> UPDATABLE_STATUSES =
-      EnumSet.of(FulfillmentFormStatus.PENDING_FULFILLMENT_FORM,
-          FulfillmentFormStatus.REJECTED_FULFILLMENT_FORM);
+    EnumSet.of(FulfillmentFormStatus.PENDING_FULFILLMENT_FORM,
+      FulfillmentFormStatus.UPDATE_FULFILLMENT_FORM,
+      FulfillmentFormStatus.REJECTED_FULFILLMENT_FORM);
 }
