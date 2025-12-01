52557b236fc2d49efb5bf346dc7f1e68bd46cae4
Yooonjeong <wkddbs3935@naver.com>
2025-12-02 01:18:31 +0900

    대리인_정렬_로직_AI_추천_성공순_등_기능_완성 : feat : properties 기본값 설정 방식 변경 https://github.com/Team-TicketMate/ticketmate-server/issues/641

--- ticketmate-member/src/main/java/com/ticketmate/backend/member/infrastructure/properties/AgentPerformanceScoreProperties.java
+++ ticketmate-member/src/main/java/com/ticketmate/backend/member/infrastructure/properties/AgentPerformanceScoreProperties.java
@@ -6,31 +6,13 @@ import org.springframework.boot.context.properties.bind.DefaultValue;
 @ConfigurationProperties(prefix = "agent.performance.score")
 public record AgentPerformanceScoreProperties(
   // 리뷰 품질 점수 배수
-  double reviewMultiplier,
-
+  @DefaultValue("10.0") double reviewMultiplier,
   // 팔로워 로그 스케일 배수
-  double followerMultiplier,
-
+  @DefaultValue("3.0") double followerMultiplier,
   // 평균 별점 점수 배수
-  double ratingMultiplier,
-
+  @DefaultValue("5.0") double ratingMultiplier,
   // 최근 30일 성공 수 점수 배수
-  double successMultiplier,
-
+  @DefaultValue("2.5") double successMultiplier,
   // 리뷰 품질 기준점
-  double reviewQualityThreshold
-) {
-  public AgentPerformanceScoreProperties(
-    @DefaultValue("10.0") double reviewMultiplier,
-    @DefaultValue("3.0") double followerMultiplier,
-    @DefaultValue("5.0") double ratingMultiplier,
-    @DefaultValue("2.5") double successMultiplier,
-    @DefaultValue("3.0") double reviewQualityThreshold) {
-
-    this.reviewMultiplier = reviewMultiplier;
-    this.followerMultiplier = followerMultiplier;
-    this.ratingMultiplier = ratingMultiplier;
-    this.successMultiplier = successMultiplier;
-    this.reviewQualityThreshold = reviewQualityThreshold;
-  }
-}
+  @DefaultValue("3.0") double reviewQualityThreshold
+) {}
