69e57fffc049776d2f22cdf8c07ebf4c6f45f6f3
Chuseok22 <bjh59629@naver.com>
2025-12-09 10:26:35 +0900

    채팅방_생성_api_파라미터_신청서_pk_기준_변경 : feat : 신청서 수락 시 채팅방 자동 생성을 위한 이벤트 핸들러 추가 https://github.com/Team-TicketMate/ticketmate-server/issues/672

---
+++ ticketmate-chat/src/main/java/com/ticketmate/backend/chat/application/handler/ApplicationFormAcceptedEventHandler.java
@@ -0,0 +1,22 @@
+package com.ticketmate.backend.chat.application.handler;
+
+import com.ticketmate.backend.applicationform.core.evnet.ApplicationFormAcceptedEvent;
+import com.ticketmate.backend.chat.application.service.ChatRoomService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class ApplicationFormAcceptedEventHandler {
+
+  private final ChatRoomService chatRoomService;
+
+  @EventListener
+  public void acceptedEventHandle(ApplicationFormAcceptedEvent event) {
+    String chatRoomId = chatRoomService.generateChatRoom(event.applicationFormId());
+    log.debug("신청서: {}에 대한 채팅방: {} 생성 완료", event.applicationFormId(), chatRoomId);
+  }
+}
