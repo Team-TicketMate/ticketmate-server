ebaf6ddd2841e95b63fc558f0c13b8769f5ed7db
정우혁 <93671010+mr6208@users.noreply.github.com>
2025-12-30 18:20:08 +0900

    대리인_및_의뢰인의_티켓팅_성공내역_생성_및_조회_기능_추가 : feat : {성공내역 모듈 추가} https://github.com/Team-TicketMate/ticketmate-server/issues/646

---
+++ ticketmate-successhistory/src/test/java/com/ticketmate/backend/TicketmateSuccesshistoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ticketmate.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketmateSuccesshistoryApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
